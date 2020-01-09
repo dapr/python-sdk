85d8cb752271ac2d9ea5888e7e3043c1d1afc746
Young Bu Park <youngp@microsoft.com>
2020-01-09 11:06:26 -0800

    Add settings

---
+++ dapr/conf/__init__.py
@@ -0,0 +1,15 @@
+from dapr.conf import global_settings
+import os
+
+class Settings:
+    def __init__(self):
+        for setting in dir(global_settings):
+            if setting.issupper():
+                default_value = getattr(global_settings, setting)
+                env_variable = os.environ.get(setting)
+                setattr(self, setting, env_variable or default_value)
+
+    def __getattr__(self, name):
+        return getattr(self, name)
+
+settings = Settings()
---
+++ dapr/conf/global_settings.py
@@ -0,0 +1,9 @@
+"""
+Default environment settings which will be overriden by environment variables
+"""
+
+HTTP_APP_PORT=3000
+GRPC_APP_PORT=50000
+
+DAPR_HTTP_PORT=3500
+DAPR_GRPC_PORT=52918
