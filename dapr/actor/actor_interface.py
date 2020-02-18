fbb6720442769a7d9a0f5fabd37cdd4758aa31f4
Young Bu Park <youngp@microsoft.com>
2020-02-17 16:35:25 -0800

    refactor ActorInterface

--- dapr/actor/actor_interface.py
+++ dapr/actor/actor_interface.py
@@ -9,27 +9,63 @@ from abc import ABC
 from dapr.actor.runtime.methodcontext import ActorMethodContext
 
 class ActorInterface(ABC):
-    @classmethod
-    def get_dispatchable_attrs(cls):
-        # Find all user actor interfaces derived from ActorInterface
-        actor_interfaces = []
-        for cl in cls.mro():
-            if cl.__base__ == ActorInterface:
-                actor_interfaces.append(cl)        
-        # Find all dispatchable attributes
-        dispatch_map = {}
-        for user_actor_cls in actor_interfaces:
-            for attr, v in user_actor_cls.__dict__.items():
-                if attr.startswith('_') or not callable(v):
-                    continue
-                actor_method_name = getattr(v, '__actormethod__') if hasattr(v, '__actormethod__') else attr
-                dispatch_map[actor_method_name] = ActorMethodContext.create_for_actor(attr)
+    """A base class that Dapr Actor inherits.
 
-        return dispatch_map
+    Actor requires to inherit ActorInterface as a base class.
+    
+    Example::
 
-def actormethod(name=None):
+        class DaprActor(ActorInterface):
+            def do_actor_method1(self, param):
+                ...
+
+            def do_actor_method2(self, param):
+                ...
+    """
+    ...
+
+def actormethod(name: str=None):
+    """Decorate actor method to define the method invoked by the remote actor.
+    
+    This allows users to call the decorated name via the proxy client.
+
+    Example::
+
+        class DaprActor(ActorInterface):
+            @actormethod(name='DoActorCall')
+            def do_actor_call(self, param):
+                ...
+
+    """
     def wrapper(funcobj):
         funcobj.__actormethod__ = name
         funcobj.__isabstractmethod__ = True
         return funcobj
     return wrapper
+
+def get_dispatchable_attrs(actor: object) -> dict:
+    """Get the list of dispatchable attributes from actor.
+
+    :param object actor: The actor object which inherits :class:`ActorInterface`
+    :returns: The map from attribute to actor method
+    :rtype: dict
+    """
+    # Find all user actor interfaces derived from ActorInterface
+    actor_interfaces = []
+    for cl in actor.mro():
+        if cl.__base__ == ActorInterface:
+            actor_interfaces.append(cl)
+    
+    if len(actor_interfaces):
+        raise ValueError(f'{actor.__name__} has not inherited from ActorInterface')
+
+    # Find all dispatchable attributes
+    dispatch_map = {}
+    for user_actor_cls in actor_interfaces:
+        for attr, v in user_actor_cls.__dict__.items():
+            if attr.startswith('_') or not callable(v):
+                continue
+            actor_method_name = getattr(v, '__actormethod__') if hasattr(v, '__actormethod__') else attr
+            dispatch_map[actor_method_name] = ActorMethodContext.create_for_actor(attr)
+
+    return dispatch_map
