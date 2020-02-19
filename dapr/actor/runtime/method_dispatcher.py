3171824a794fbf6b072cc382e4092a83c995f7c2
Young Bu Park <youngp@microsoft.com>
2020-02-19 07:09:32 -0800

    add helpers to dispatcher

--- dapr/actor/runtime/method_dispatcher.py
+++ dapr/actor/runtime/method_dispatcher.py
@@ -13,16 +13,23 @@ class ActorMethodDispatcher:
     def __init__(self, type_info: ActorTypeInformation):
         self._dispatch_mapping = get_dispatchable_attrs(type_info.implementation_type)
 
-    def get_params(self, name: str) -> dict:
-        if name not in self._dispatch_mapping:
-            raise AttributeError(
-                f'no method {name} is in dispatch mapping')
-
-        return self._dispatch_mapping[name].params
-
     def dispatch(self, actor: Actor, name: str, *args, **kwargs):
+        self._check_name_exist(name)
+        return getattr(actor, self._dispatch_mapping[name].method_name)(*args, **kwargs)
+
+    def get_arg_names(self, name: str) -> list:
+        self._check_name_exist(name)
+        return self._dispatch_mapping[name].arg_names
+
+    def get_arg_types(self, name: str) -> list:
+        self._check_name_exist(name)
+        return self._dispatch_mapping[name].arg_types
+
+    def get_return_type(self, name: str) -> type:
+        self._check_name_exist(name)
+        return self._dispatch_mapping[name].return_types
+    
+    def _check_name_exist(self, name: str):
         if name not in self._dispatch_mapping:
             raise AttributeError(
                 f'type object {self.__class__.__name__} has no method {name}')
-
-        return getattr(actor, self._dispatch_mapping[name].method_name)(*args, **kwargs)
