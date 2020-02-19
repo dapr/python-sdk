3bd80812bec46b6016856d29642b941b2b43323e
Young Bu Park <youngp@microsoft.com>
2020-02-19 07:08:35 -0800

    add argument name and params getter

--- dapr/actor/actor_interface.py
+++ dapr/actor/actor_interface.py
@@ -43,6 +43,30 @@ def actormethod(name: str=None):
         return funcobj
     return wrapper
 
+def get_class_method_args(fn: type) -> list:
+    args = fn.__code__.co_varnames[:fn.__code__.co_argcount]
+
+    # Exclude self, cls arguments
+    if args[0] == 'self' or args[0] == 'cls':
+        args = args[1:]
+    return list(args)
+
+def get_method_arg_types(fn: type) -> list:
+    annotations = getattr(fn, '__annotations__')
+    args = get_class_method_args(fn)
+    arg_types = []
+    for arg_name in args:
+        arg_type = object if arg_name not in annotations else annotations[arg_name]
+        arg_types.append(arg_type)
+    return arg_types
+
+def get_method_return_types(fn: type) -> type:
+    annotations = getattr(fn, '__annotations__')
+    if not annotations['return']:
+        return object
+    else:
+        return annotations['return']
+
 def get_dispatchable_attrs(actor: object) -> dict:
     """Get the list of dispatchable attributes from actor.
 
@@ -66,9 +90,12 @@ def get_dispatchable_attrs(actor: object) -> dict:
             if attr.startswith('_') or not callable(v):
                 continue
             actor_method_name = getattr(v, '__actormethod__') if hasattr(v, '__actormethod__') else attr
+
             dispatch_map[actor_method_name] = {
                 'method_name': attr,
-                'params': getattr(v, '__annotations__'),
+                'arg_names': get_class_method_args(v),
+                'arg_types': get_method_arg_types(v),
+                'return_types': get_method_return_types(v)
             }
 
     return dispatch_map
