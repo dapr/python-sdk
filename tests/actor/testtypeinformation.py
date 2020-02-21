9003cbb07d14c3b1d99c27198c84ecd51acf18f1
Young Bu Park <youngp@microsoft.com>
2020-02-20 22:17:46 -0800

    initial commit of tests

---
+++ tests/actor/testtypeinformation.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT License.
+"""
+
+import unittest
+
+from datetime import timedelta
+from dapr.actor.runtime.typeinformation import *
+
+class ActorTypeInformationTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_is_dapr_actor_true(self):
+        pass
+
+    def test_is_dapr_actor_false(self):
+        pass
+
+    def test_get_non_actor_parent_type_returns_non_actor(self):
+        pass
+
+    def test_get_non_actor_parent_type_returns_actor(self):
+        pass
+
+    def test_get_actor_interfaces(self):
+        pass
+
+    def test_actor_type_name(self):
+        pass
+
+    def test_implementation_type_returns_correct_type(self):
+        pass
+
+    def test_actor_interfaces_returns_actor_classes(self):
+        pass
+
+        
+if __name__ == '__main__':
+    unittest.main()
