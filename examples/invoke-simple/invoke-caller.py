5b668c6e37d016f8211c1fdaef27ac9cd9e3aa9a
Young Bu Park <youngp@microsoft.com>
2020-04-24 18:11:43 -0700

    fix examples

--- examples/invoke-simple/invoke-caller.py
+++ examples/invoke-simple/invoke-caller.py
@@ -1,6 +1,8 @@
 import os
 
 import grpc
+
+from dapr.proto.common.v1 import common_pb2 as commonv1pb
 from dapr.proto.dapr.v1 import dapr_pb2 as dapr_messages
 from dapr.proto.dapr.v1 import dapr_pb2_grpc as dapr_services
 from dapr.proto.common.v1 import common_pb2
@@ -14,9 +16,23 @@ channel = grpc.insecure_channel(f"localhost:{port}")
 client = dapr_services.DaprStub(channel)
 print(f"Started gRPC client on DAPR_GRPC_PORT: {port}")
 
-data1=Any(value='ACTION=1'.encode('utf-8'))
-message1 = common_pb2.InvokeRequest(method='my-method', data=data1)
-response = client.InvokeService(dapr_messages.InvokeServiceRequest(id="invoke-receiver", message=message1))
-print(response.data.value)
+test_message = Any()
+test_message.Pack(commonv1pb.DataWithContentType(
+    content_type="text/plain; charset=UTF-8",
+    body='INVOKE_RECEIVED'.encode('utf-8')
+))
+
+req = dapr_messages.InvokeServiceRequest(
+    id="invoke-receiver",
+    message=common_pb2.InvokeRequest(method='my-method', data=test_message)
+)
+
+response = client.InvokeService(req)
+
+if response.data.Is(commonv1pb.DataWithContentType.DESCRIPTOR):
+    resp_data = commonv1pb.DataWithContentType()
+    response.data.Unpack(resp_data)
+    print(resp_data.content_type)
+    print(resp_data.body)
 
 channel.close()
