78d14269bf9566204fc5dfa00899861b94df6dde
Pruthvidhar R Dhodda <60198385+pruthvidhodda@users.noreply.github.com>
2020-04-27 22:58:28 -0700

    Add a script to generate gRPC client code/stubs from dapr proto files (#28)
    * Add script for proto buf and grpc interface generation
    * Update example.py and invoke-simple samples
    * Add docs on how to use the script
    
    Co-authored-by: Young Bu Park <youngp@microsoft.com>

--- examples/invoke-simple/invoke-caller.py
+++ examples/invoke-simple/invoke-caller.py
@@ -1,8 +1,11 @@
 import os
 
 import grpc
-import dapr_pb2 as dapr_messages
-import dapr_pb2_grpc as dapr_services
+
+from dapr.proto.common.v1 import common_pb2 as commonv1pb
+from dapr.proto.dapr.v1 import dapr_pb2 as dapr_messages
+from dapr.proto.dapr.v1 import dapr_pb2_grpc as dapr_services
+from dapr.proto.common.v1 import common_pb2
 
 from google.protobuf.any_pb2 import Any
 
@@ -13,8 +16,25 @@ channel = grpc.insecure_channel(f"localhost:{port}")
 client = dapr_services.DaprStub(channel)
 print(f"Started gRPC client on DAPR_GRPC_PORT: {port}")
 
-data = Any(value='ACTION=1'.encode('utf-8'))
-response = client.InvokeService(dapr_messages.InvokeServiceEnvelope(id="invoke-receiver", method="my_method", data=data))
-print(response.data.value)
+# Create a typed message with content type and body
+test_message = Any()
+test_message.Pack(commonv1pb.DataWithContentType(
+    content_type="text/plain; charset=UTF-8",
+    body='INVOKE_RECEIVED'.encode('utf-8')
+))
+
+# Invoke the method 'my-method' on receiver 
+req = dapr_messages.InvokeServiceRequest(
+    id="invoke-receiver",
+    message=common_pb2.InvokeRequest(method='my-method', data=test_message)
+)
+response = client.InvokeService(req)
+
+# Print the response
+if response.data.Is(commonv1pb.DataWithContentType.DESCRIPTOR):
+    resp_data = commonv1pb.DataWithContentType()
+    response.data.Unpack(resp_data)
+    print(resp_data.content_type)
+    print(resp_data.body)
 
 channel.close()
---
+++ protobuf.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# ------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------------------------------
+
+COMMON="common"
+DAPR="dapr"
+DAPR_CLIENT="daprclient"
+
+# Path to store output
+PROTO_PATH="src/dapr/proto"
+SRC=src
+
+# Http request CLI
+HTTP_REQUEST_CLI=curl
+
+
+checkHttpRequestCLI() {
+    if type "curl" > /dev/null; then
+        HTTP_REQUEST_CLI=curl
+    elif type "wget" > /dev/null; then
+        HTTP_REQUEST_CLI=wget
+    else
+        echo "Either curl or wget is required"
+        exit 1
+    fi
+}
+
+downloadFile() {
+
+    FILE_NAME=$1
+    FILE_PATH="${PROTO_PATH}/${FILE_NAME}/v1"
+
+    # URL for proto file
+    PROTO_URL="https://raw.githubusercontent.com/dapr/dapr/master/dapr/proto/${FILE_NAME}/v1/${FILE_NAME}.proto"
+
+    mkdir -p "${FILE_PATH}"
+
+    echo "Downloading $PROTO_URL ..."
+    if [ "$HTTP_REQUEST_CLI" == "curl" ]; then
+        cd ${FILE_PATH}
+        curl -SsL "$PROTO_URL" -o "${FILE_NAME}.proto"
+        cd ../../../../..
+    else
+        wget -q -P "$PROTO_URL" "${FILE_PATH}/${FILE_NAME}.proto"
+    fi
+
+    if [ ! -e "${FILE_PATH}/${FILE_NAME}.proto" ]; then
+        echo "failed to download $PROTO_URL ..."
+        ret_val=$FILE_NAME
+        exit 1
+    fi
+}
+
+generateGrpc() {
+
+    FILE_NAME=$1
+    FILE_PATH="${PROTO_PATH}/${FILE_NAME}/v1"
+
+    python3 -m grpc_tools.protoc -I ${SRC} --python_out=${SRC} --grpc_python_out=${SRC} ${FILE_PATH}/${FILE_NAME}.proto
+
+    if [ ! -e "${FILE_PATH}/${FILE_NAME}_pb2.py" ]; then
+        echo "failed to generate proto buf $FILE_NAME"
+        ret_val=$FILE_NAME
+        exit 1
+    fi
+}
+
+fail_trap() {
+    result=$?
+    if [ $result != 0 ]; then
+        echo "Failed to generate gRPC interface and proto buf: $ret_val"
+    fi
+    cleanup
+    exit $result
+}
+
+cleanup() {
+    find $PROTO_PATH -type f -name '*.proto' -delete
+}
+
+generateGrpcSuccess() {
+    export PYTHONPATH=`pwd`/$SRC
+    echo -e "\ngRPC interface and proto buf generated successfully!"
+}
+
+# -----------------------------------------------------------------------------
+# main
+# -----------------------------------------------------------------------------
+trap "fail_trap" EXIT
+
+checkHttpRequestCLI
+downloadFile $COMMON
+generateGrpc $COMMON
+downloadFile $DAPR
+generateGrpc $DAPR
+downloadFile $DAPR_CLIENT
+generateGrpc $DAPR_CLIENT
+cleanup
+
+generateGrpcSuccess
+
