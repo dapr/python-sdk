3f7d5d708adffd1db3eaae771d57ce391657807d
Young Bu Park <youngp@microsoft.com>
2020-05-18 01:45:41 -0700

    update pkg description

--- setup.py
+++ setup.py
@@ -7,5 +7,19 @@ Licensed under the MIT License.
 
 from setuptools import setup
 
-if __name__ == "__main__":
-    setup()
+description = 'Dapr SDK for Python'
+long_description = '''
+Dapr is a portable, serverless, event-driven runtime that makes it easy for developers to 
+build resilient, stateless and stateful microservices that run on the cloud and edge and 
+embraces the diversity of languages and developer frameworks.
+
+Dapr codifies the best practices for building microservice applications into open, 
+independent, building blocks that enable you to build portable applications with the language 
+and framework of your choice. Each building block is independent and you can use one, some, 
+or all of them in your application.
+'''.lstrip()
+
+setup(
+    description=description,
+    long_description=long_description,
+)
