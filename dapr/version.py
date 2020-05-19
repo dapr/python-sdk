70eae2902c8423b5745f0d4db25da33d861ef881
Young Bu Park <youngp@microsoft.com>
2020-05-18 22:23:06 -0700

    Setup dev pkg

--- dapr/version.py
+++ dapr/version.py
@@ -5,8 +5,4 @@ Copyright (c) Microsoft Corporation.
 Licensed under the MIT License.
 """
 
-from .version import __version__
-
-__all__ = [
-    '__version__',
-]
+__version__ = "0.5.0a0"
--- setup.py
+++ setup.py
@@ -5,8 +5,27 @@ Copyright (c) Microsoft Corporation.
 Licensed under the MIT License.
 """
 
-from setuptools import setup
+import re
+import os
 
+from setuptools import setup
+from subprocess import check_output as run
+
+exec(open('dapr/version.py').read())
+
+
+def parse_version(v):
+    versionPattern = r'\d+(=?\.(\d+(=?\.(\d+)*)*)*)*'
+    regexMatcher = re.compile(versionPattern)
+    return regexMatcher.search(v).group(0)
+
+
+def is_release():
+    tagged_version = run(['git', 'describe', '--tags']).decode('utf-8').strip()[1:]
+    return tagged_version == __version__
+
+
+name = 'dapr'
 description = 'Dapr SDK for Python'
 long_description = '''
 Dapr is a portable, serverless, event-driven runtime that makes it easy for developers to
@@ -18,8 +37,21 @@ independent, building blocks that enable you to build portable applications with
 and framework of your choice. Each building block is independent and you can use one, some,
 or all of them in your application.
 '''.lstrip()
+version = __version__
+
+# Get build number from GITHUB_RUN_NUMBER environment variable
+build_number = os.environ.get('GITHUB_RUN_NUMBER', '0')
+
+if not is_release():
+    name += '-dev'
+    version = f'{parse_version(__version__)}.dev{build_number}'
+    description = 'Dapr SDK for python'
+    long_description = 'This is the development build.'
+
 
 setup(
+    name=name,
+    version=version,
     description=description,
     long_description=long_description,
 )
