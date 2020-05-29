2ca5a5bffaef10baf0aa8adfbbb15537ffb69c4f
chinzhiweiblank <chinzhiweiblank@gmail.com>
2020-05-29 22:00:48 +0800

    Move actor

--- flask_dapr/__init__.py
+++ flask_dapr/__init__.py
@@ -5,7 +5,7 @@ Copyright (c) Microsoft Corporation.
 Licensed under the MIT License.
 """
 
-from flask_dapr.actor import DaprActor
+from .actor.actor import DaprActor
 from .version import __version__
 
 __all__ = ['DaprActor', '__version__', ]
