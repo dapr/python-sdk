e7ea1d4ff8caa022593a7d7b29125d26f0461341
Young Bu Park <youngp@microsoft.com>
2020-06-28 05:53:05 -0700

    update examples

--- examples/invoke-simple/invoke-receiver.py
+++ examples/invoke-simple/invoke-receiver.py
@@ -1,39 +1,14 @@
-import os
-import time
-import grpc
+from dapr import App, CallbackResponse
 
-from concurrent import futures
+app = App()
 
-from dapr.proto import appcallback_service_v1, common_v1
-from google.protobuf.any_pb2 import Any
+@app.method(name='my-method')
+def mymethod(metadata, data, content_type, *http_args) -> CallbackResponse:
+    print(metadata, flush=True)
+    print(data, flush=True)
 
-# Our server methods
-class AppCallback(appcallback_service_v1.AppCallbackServicer):
-    def OnInvoke(self, request, context):
-        data=None
-        content_type=""
-        if request.method == 'my-method':
-            data = Any(value='INVOKE_RECEIVED'.encode('utf-8'))
-            content_type = "text/plain; charset=UTF-8"
-        else:
-            data = Any(value='unsupported methods'.encode('utf-8'))
-            content_type = "text/plain; charset=UTF-8"
+    return CallbackResponse(b'INVOKE_RECEIVED', "text/plain; charset=UTF-8")
 
-        # Return response to caller
-        return common_v1.InvokeResponse(data=data, content_type=content_type)
+app.daprize()
 
-# Create a gRPC server
-server = grpc.server(futures.ThreadPoolExecutor(max_workers = 10))
-appcallback_service_v1.add_AppCallbackServicer_to_server(AppCallback(), server)
-
-# Start the gRPC server
-print('Starting server. Listening on port 50051.')
-server.add_insecure_port('[::]:50051')
-server.start()
-
-# Since server.start() doesn't block, we need to do a sleep loop
-try:
-    while True:
-        time.sleep(86400)
-except KeyboardInterrupt:
-    server.stop(0)
+app.wait_until_stop()
--- examples/pubsub-simple/subscriber.py
+++ examples/pubsub-simple/subscriber.py
@@ -1,42 +1,11 @@
-import os
-from concurrent import futures
-import time
+from cloudevents.sdk.event import v1
+from dapr import App
 
-import grpc
-from dapr.proto import appcallback_v1, appcallback_service_v1, common_v1
+app = App()
 
-from google.protobuf import empty_pb2
+@app.subscriber(topic='TOPIC_A')
+def mytopic(event: v1.Event) -> None:
+    print(event.Data(),flush=True)
 
-import logging
-logging.basicConfig(level=logging.INFO, format='%(message)s')
-logger = logging.getLogger(__name__)
-
-
-# Our server methods
-class AppCallback(appcallback_service_v1.AppCallbackServicer):
-    def ListTopicSubscriptions(self, request, context):
-        # Dapr will call this method to get the list of topics the app
-        # wants to subscribe to. In this example, we are telling Dapr
-        # To subscribe to a topic named TOPIC_A
-        return appcallback_v1.ListTopicSubscriptionsResponse(topics=['TOPIC_A'])
-
-    def OnTopicEvent(self, request, context):
-        logging.info("Event received!!")
-        return empty_pb2.Empty()
-
-
-# Create a gRPC server
-server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-appcallback_service_v1.add_AppCallbackServicer_to_server(AppCallback(), server)
-
-# Start the gRPC server
-print('Starting server. Listening on port 50051.')
-server.add_insecure_port('[::]:50051')
-server.start()
-
-# Since server.start() doesn't block, we need to do a sleep loop
-try:
-    while True:
-        time.sleep(86400)
-except KeyboardInterrupt:
-    server.stop(0)
+app.daprize()
+app.wait_until_stop()
