5ef27bb933ae49612a5e89f2127e3901c5243ac3
Young Bu Park <youngp@microsoft.com>
2020-07-08 17:15:16 -0700

    fix test errors

--- dapr/clients/__init__.py
+++ dapr/clients/__init__.py
@@ -5,12 +5,14 @@ Copyright (c) Microsoft Corporation.
 Licensed under the MIT License.
 """
 
+from dapr.clients.base import DaprActorClientBase
 from dapr.clients.exceptions import DaprInternalError, ERROR_CODE_UNKNOWN
 from dapr.clients.grpc.client import DaprClient
 from dapr.clients.http.dapr_actor_http_client import DaprActorHttpClient
 
 __all__ = [
     'DaprClient',
+    'DaprActorClientBase',
     'DaprActorHttpClient',
     'DaprInternalError',
     'ERROR_CODE_UNKNOWN',
