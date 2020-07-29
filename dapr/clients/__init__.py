dcbf813582d50c60f25382c98d40555387fa5eb0
Young Bu Park <youngp@microsoft.com>
2020-07-28 21:32:48 -0700

    revert

--- dapr/clients/__init__.py
+++ dapr/clients/__init__.py
@@ -5,9 +5,9 @@ Copyright (c) Microsoft Corporation.
 Licensed under the MIT License.
 """
 
+from dapr.clients.base import DaprActorClientBase
 from dapr.clients.exceptions import DaprInternalError, ERROR_CODE_UNKNOWN
 from dapr.clients.grpc.client import DaprClient
-from dapr.clients.base import DaprActorClientBase
 from dapr.clients.http.dapr_actor_http_client import DaprActorHttpClient
 
 __all__ = [
