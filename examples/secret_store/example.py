e15f2a334cfde7929e864a98816f74f624bbac62
Mukundan Sundararajan <musundar@microsoft.com>
2020-09-21 10:36:44 -0700

    Add allowlist example for secrets. Fix localstore config.

--- examples/secret_store/example.py
+++ examples/secret_store/example.py
@@ -7,8 +7,16 @@ from dapr.clients import DaprClient
 
 with DaprClient() as d:
     key = 'secretKey'
+    randomKey = "random"
     storeName = 'localsecretstore'
 
     resp = d.get_secret(store_name=storeName, key=key)
     print('Got!')
-    print(resp._secret)
+    print(resp.secret)
+    try:
+        resp = d.get_secret(store_name=storeName, key=randomKey)
+        print('Got!')
+        print(resp.secret)
+    except:
+        print("Got expected error for accessing random key")
+
