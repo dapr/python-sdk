0d8b9d53b5a8f37d47c0f046c7d4367a4bf892b9
Artur Souza <artursouza.ms@outlook.com>
2021-01-26 19:02:22 -0800

    Adds getBulkSecret.

--- examples/secret_store/example.py
+++ examples/secret_store/example.py
@@ -13,6 +13,10 @@ with DaprClient() as d:
     resp = d.get_secret(store_name=storeName, key=key)
     print('Got!')
     print(resp.secret)
+    resp = d.get_bulk_secret(store_name=storeName)
+    print('Got!')
+    # Converts dict into sorted list of tuples for deterministic output.
+    print(sorted(resp.secrets.items()))
     try:
         resp = d.get_secret(store_name=storeName, key=randomKey)
         print('Got!')
