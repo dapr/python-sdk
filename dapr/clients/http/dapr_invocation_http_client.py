d84c61154bbfa219a40874f3544a6f918251e434
Charlie Stanley <charlie.stanley@microsoft.com>
2021-08-30 16:16:32 -0700

    Fix event loop invoke change in python >=3.8

--- dapr/clients/http/dapr_invocation_http_client.py
+++ dapr/clients/http/dapr_invocation_http_client.py
@@ -93,4 +93,7 @@ class DaprInvocationHttpClient:
                 resp_data.headers[key] = r.headers.getall(key)  # type: ignore
             return resp_data
 
-        return asyncio.run(make_request())
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        return loop.run_until_complete(make_request())
