3f38bd64db8fa2e407584342b621111936075bd8
Phil Kedy <phil.kedy@gmail.com>
2021-08-30 17:18:24 -0400

    Undoing change to example

--- examples/pubsub-simple/subscriber.py
+++ examples/pubsub-simple/subscriber.py
@@ -1,11 +1,11 @@
 from cloudevents.sdk.event import v1
-from dapr.ext.grpc import App, Rule
+from dapr.ext.grpc import App
 
 import json
 
 app = App()
 
-@app.subscribe(pubsub_name='pubsub', topic='TOPIC_A', rule=Rule("event.type == \"test\"", 0))
+@app.subscribe(pubsub_name='pubsub', topic='TOPIC_A')
 def mytopic(event: v1.Event) -> None:
     data = json.loads(event.Data())
     print(f'Subscriber received: id={data["id"]}, message="{data["message"]}", content_type="{event.content_type}"',flush=True)
