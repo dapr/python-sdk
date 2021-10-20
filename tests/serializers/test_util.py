bbc7b822db0aa25991d6c8c678d021e0308b8a17
Bernd Verst <4535280+berndverst@users.noreply.github.com>
2021-10-20 13:26:39 -0700

    Fix Dapr duration string parsing + new tests (#288)
    * Fix Dapr duration string parsing + new tests
    
    * address linter failure
    
    * Address code review

--- tests/serializers/test_util.py
+++ tests/serializers/test_util.py
@@ -6,9 +6,11 @@ Licensed under the MIT License.
 """
 
 import unittest
+import json
 from datetime import timedelta
 
 from dapr.serializers.util import convert_from_dapr_duration, convert_to_dapr_duration
+from dapr.serializers.json import DaprJSONDecoder
 
 
 class UtilTests(unittest.TestCase):
@@ -28,12 +30,23 @@ class UtilTests(unittest.TestCase):
         self.assertEqual(delta.total_seconds(), 40.0)
 
     def test_convert_invalid_duration(self):
-        delta = convert_from_dapr_duration('invalid')
-        self.assertEqual(delta.total_seconds(), 0.0)
+        with self.assertRaises(ValueError) as exeception_context:
+            convert_from_dapr_duration('invalid')
+        self.assertEqual(exeception_context.exception.args[0],
+                         "Invalid Dapr Duration format: '{}'".format('invalid'))
 
-    def test_convert_timedelta_to_dapr_duariton(self):
+    def test_convert_timedelta_to_dapr_duration(self):
         duration = convert_to_dapr_duration(timedelta(hours=4, minutes=15, seconds=40))
         self.assertEqual(duration, '4h15m40s')
+
+    def test_convert_invalid_duration_string(self):
+        TESTSTRING = '4h15m40shello'
+        with self.assertRaises(ValueError) as exeception_context:
+            convert_from_dapr_duration(TESTSTRING)
+        self.assertEqual(exeception_context.exception.args[0],
+                         "Invalid Dapr Duration format: '{}'".format(TESTSTRING))
+        decoded = json.loads(json.dumps({"somevar": TESTSTRING}), cls=DaprJSONDecoder)
+        self.assertEqual(decoded['somevar'], TESTSTRING)
 
 
 if __name__ == '__main__':
