4b215ddb351319d14fbdc0a81b7176858759467c
Willem Meints <1550763+wmeints@users.noreply.github.com>
2021-11-02 19:58:25 +0100

    Fix flake8 errors

--- ext/dapr-ext-fastapi/dapr/ext/fastapi/app.py
+++ ext/dapr-ext-fastapi/dapr/ext/fastapi/app.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 """
 Copyright (c) Microsoft Corporation and Dapr Contributors.
 Licensed under the MIT License.
@@ -20,20 +19,26 @@ class DaprApp:
         self._app = app_instance
         self._subscriptions = []
 
-        self._app.add_api_route("/dapr/subscribe", self._get_subscriptions, methods=["GET"])
-    
-    def subscribe(self, pubsub: str, topic: str, metadata: Optional[Dict[str, str]] = {}, route: Optional[str] = None):
+        self._app.add_api_route("/dapr/subscribe",
+                                self._get_subscriptions,
+                                methods=["GET"])
+
+    def subscribe(self,
+                  pubsub: str,
+                  topic: str,
+                  metadata: Optional[Dict[str, str]] = {},
+                  route: Optional[str] = None):
         """
         Subscribes to a topic on a pub/sub component.
 
-        Subscriptions made through this method will show up when you GET /dapr/subscribe. 
+        Subscriptions made through this method will show up when you GET /dapr/subscribe.
 
         Example:
-            The following sample demonstrates how to use the subscribe method to register an event handler for the 
-            application on a pub/sub component named `pubsub`.
+            The following sample demonstrates how to use the subscribe method to register an
+            event handler for the application on a pub/sub component named `pubsub`.
 
             >> app = FastAPI()
-            >> dapr_app = DaprApp(app) 
+            >> dapr_app = DaprApp(app)
             >> @dapr_app.subscribe(pubsub='pubsub', topic='some_topic', route='/some_endpoint')
             >> def my_event_handler(event_data):
             >>    pass
@@ -42,17 +47,20 @@ class DaprApp:
             pubsub: The name of the pub/sub component.
             topic: The name of the topic.
             metadata: The metadata for the subscription.
-            route: 
-                The HTTP route to register for the event subscription. By default we'll generate one that 
-                matches the pattern /events/{pubsub}/{topic}. You can override this with your own route.
-        
+            route:
+                The HTTP route to register for the event subscription. By default we'll
+                generate one that matches the pattern /events/{pubsub}/{topic}. You can
+                override this with your own route.
+
         Returns:
             The decorator for the function.
         """
         def decorator(func):
             event_handler_route = f"/events/{pubsub}/{topic}" if route is None else route
-        
-            self._app.add_api_route(event_handler_route, func, methods=["POST"])
+
+            self._app.add_api_route(event_handler_route,
+                                    func,
+                                    methods=["POST"])
 
             self._subscriptions.append({
                 "pubsubname": pubsub,
--- ext/dapr-ext-fastapi/tests/test_app.py
+++ ext/dapr-ext-fastapi/tests/test_app.py
@@ -16,6 +16,7 @@ def test_subscribe_subscription_registered():
     assert app.router.routes[0].path == "/dapr/subscribe"
     assert app.router.routes[1].route == "/events/pubsub/test"
 
+
 def test_subscribe_with_route_subscription_registered_with_custom_route():
     app = FastAPI()
     dapr_app = DaprApp(app)
@@ -30,16 +31,17 @@ def test_subscribe_with_route_subscription_registered_with_custom_route():
     assert app.router.routes[0].path == "/dapr/subscribe"
     assert app.router.routes[1].route == "/do-something"
 
+
 def test_subscribe_metadata():
     app = FastAPI()
     dapr_app = DaprApp(app)
 
-    handler_metadata = {
-        "rawPayload": "true"
-    }
+    handler_metadata = {"rawPayload": "true"}
 
-    @dapr_app.subscribe(pubsub="pubsub", topic="test", metadata=handler_metadata)
+    @dapr_app.subscribe(pubsub="pubsub",
+                        topic="test",
+                        metadata=handler_metadata)
     def event_handler(event_data):
         pass
 
-    assert(dapr_app._subscriptions[0]["metadata"]["rawPayload"]) == "true"
+    assert (dapr_app._subscriptions[0]["metadata"]["rawPayload"]) == "true"
