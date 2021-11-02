cfa7ef164f0ea6b3c6c0d41a5e2d070f1a1c68d6
Charlie Stanley <charlie.stanley@microsoft.com>
2021-11-02 15:59:09 -0700

    Migrate tests to unittest and add some tests to exercise callbacks

--- ext/dapr-ext-fastapi/tests/test_app.py
+++ ext/dapr-ext-fastapi/tests/test_app.py
@@ -1,47 +1,76 @@
 from fastapi import FastAPI
+from fastapi.testclient import TestClient
 from dapr.ext.fastapi import DaprApp
+from pydantic import BaseModel
+
+import unittest
+
+class Message(BaseModel):
+    body: str
+
+class DaprAppTest(unittest.TestCase):
+    def setUp(self):
+        self.app = FastAPI()
+        self.dapr_app = DaprApp(self.app)
+
+        self.client = TestClient(self.app)
 
 
-def test_subscribe_subscription_registered():
-    app = FastAPI()
-    dapr_app = DaprApp(app)
+    def test_subscribe_subscription_registered(self):
 
-    @dapr_app.subscribe(pubsub="pubsub", topic="test")
-    def event_handler(event_data):
-        pass
+        @self.dapr_app.subscribe(pubsub="pubsub", topic="test")
+        def event_handler(event_data: Message):
+            return "default route"
 
-    assert len(dapr_app._subscriptions) == 1
-    assert len(app.router.routes) == 2
+        self.assertEqual(len(self.dapr_app._subscriptions), 1)
 
-    assert app.router.routes[0].path == "/dapr/subscribe"
-    assert app.router.routes[1].route == "/events/pubsub/test"
+        self.assertIn("/dapr/subscribe", [route.path for route in self.app.router.routes])
+        self.assertIn("/events/pubsub/test", [route.path for route in self.app.router.routes])
+
+        response = self.client.get("/dapr/subscribe")
+        self.assertEqual([{'pubsubname': 'pubsub', 'topic': 'test', 'route': '/events/pubsub/test', 'metadata': {}}], response.json())
+        
+        response = self.client.post("/events/pubsub/test", json={"body": "new message"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text, '"default route"')
+
+    def test_subscribe_with_route_subscription_registered_with_custom_route(self):
+
+        @self.dapr_app.subscribe(pubsub="pubsub", topic="test", route="/do-something")
+        def event_handler(event_data: Message):
+            return "custom route"
+
+        self.assertEqual(len(self.dapr_app._subscriptions), 1)
+
+        self.assertIn("/dapr/subscribe", [route.path for route in self.app.router.routes])
+        self.assertIn("/do-something", [route.path for route in self.app.router.routes])
+        
+        response = self.client.get("/dapr/subscribe")
+        self.assertEqual([{'pubsubname': 'pubsub', 'topic': 'test', 'route': '/do-something', 'metadata': {}}], response.json())
+        
+        response = self.client.post("/do-something", json={"body": "new message"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text, '"custom route"')
 
 
-def test_subscribe_with_route_subscription_registered_with_custom_route():
-    app = FastAPI()
-    dapr_app = DaprApp(app)
+    def test_subscribe_metadata(self):
 
-    @dapr_app.subscribe(pubsub="pubsub", topic="test", route="/do-something")
-    def event_handler(event_data):
-        pass
+        handler_metadata = {"rawPayload": "true"}
 
-    assert len(dapr_app._subscriptions) == 1
-    assert len(app.router.routes) == 2
+        @self.dapr_app.subscribe(pubsub="pubsub",
+                            topic="test",
+                            metadata=handler_metadata)
+        def event_handler(event_data: Message):
+            return "custom metadata"
 
-    assert app.router.routes[0].path == "/dapr/subscribe"
-    assert app.router.routes[1].route == "/do-something"
+        self.assertEqual((self.dapr_app._subscriptions[0]["metadata"]["rawPayload"]), "true")
+        
+        response = self.client.get("/dapr/subscribe")
+        self.assertEqual([{'pubsubname': 'pubsub', 'topic': 'test', 'route': '/events/pubsub/test', 'metadata': {"rawPayload": "true"}}], response.json())
+        
+        response = self.client.post("/events/pubsub/test", json={"body": "new message"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text, '"custom metadata"')
 
-
-def test_subscribe_metadata():
-    app = FastAPI()
-    dapr_app = DaprApp(app)
-
-    handler_metadata = {"rawPayload": "true"}
-
-    @dapr_app.subscribe(pubsub="pubsub",
-                        topic="test",
-                        metadata=handler_metadata)
-    def event_handler(event_data):
-        pass
-
-    assert (dapr_app._subscriptions[0]["metadata"]["rawPayload"]) == "true"
+if __name__ == '__main__':
+    unittest.main()
