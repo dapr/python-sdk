f05a17a4334d7bb715828d31ca79e895e7bfc16f
Wim <wim.vdc@hotmail.com>
2021-11-14 09:59:55 +0100

    Changed asyncio invocation in http_client invoke_method to re-use exiting event loop if one exists (see issue #295)
    Signed-off-by: Wim <wim.vdc@hotmail.com>
    Signed-off-by: Bernd Verst <4535280+berndverst@users.noreply.github.com>

--- dapr/clients/http/dapr_invocation_http_client.py
+++ dapr/clients/http/dapr_invocation_http_client.py
@@ -101,7 +101,10 @@ class DaprInvocationHttpClient:
                 resp_data.headers[key] = r.headers.getall(key)  # type: ignore
             return resp_data
 
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-
-        return loop.run_until_complete(make_request())
+        try:
+            asyncio.get_event_loop()
+            return asyncio.create_task(make_request())
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            return loop.run_until_complete(make_request())
