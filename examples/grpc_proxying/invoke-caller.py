d27d637d3a8ae9ce40b3c8210c5b99f765641dcf
Omid Bakhshandeh <omid@verneek.com>
2021-11-17 08:56:21 -0500

    removing comments

--- examples/grpc_proxying/invoke-caller.py
+++ examples/grpc_proxying/invoke-caller.py
@@ -4,7 +4,6 @@ import logging
 import grpc
 import helloworld_service_pb2_grpc
 from helloworld_service_pb2 import HelloRequest, HelloReply
-from dapr.clients import DaprClient
 import json, time
 
 async def run() -> None:
@@ -14,32 +13,8 @@ async def run() -> None:
         response = await stub.SayHello(request=HelloRequest(name='you'), metadata=metadata)
     print("Greeter client received: " + response.message)
 
-
-def publish() -> None:
-    with DaprClient() as d:
-        id=0
-        while True:
-            id+=1
-            req_data = {
-                'id': id,
-                'message': 'hello world'
-            }
-
-            # Create a typed message with content type and body
-            resp = d.publish_event(
-                pubsub_name='pubsub',
-                topic_name='TOPIC_A',
-                data=json.dumps(req_data),
-                data_content_type='application/json',
-            )
-
-            # Print the request
-            print(req_data, flush=True)
-            time.sleep(2)
-
-
 if __name__ == '__main__':
     print('I am in main')
     logging.basicConfig()
     asyncio.run(run())
-    publish()
+
--- examples/grpc_proxying/invoke-reciever.py
+++ examples/grpc_proxying/invoke-reciever.py
@@ -1,11 +1,9 @@
-import asyncio
 import logging
 
 import grpc
 import helloworld_service_pb2_grpc
 from helloworld_service_pb2 import HelloRequest, HelloReply
 from dapr.ext.grpc import App
-from cloudevents.sdk.event import v1
 import json
 
 class HelloWorldService(helloworld_service_pb2_grpc.HelloWorldService):
@@ -15,15 +13,6 @@ class HelloWorldService(helloworld_service_pb2_grpc.HelloWorldService):
         print('hello in servicer')
         return HelloReply(message='Hello, %s!' % request.name)
 
-
-# async def serve() -> None:
-#     server = grpc.aio.server()
-#     helloworld_service_pb2_grpc.add_HelloWorldServiceServicer_to_server(HelloWorldService(), server)
-#     listen_addr = '127.0.0.1:50051'
-#     server.add_insecure_port(listen_addr)
-#     logging.info("Starting server on this: %s", listen_addr)
-#     await server.start()
-#     await server.wait_for_termination()
 app = App()
 
 @app.subscribe(pubsub_name='pubsub', topic='TOPIC_A')
@@ -35,6 +24,5 @@ def mytopic(event: v1.Event) -> None:
 if __name__ == '__main__':
     print('starting the HelloWorld Service')
     logging.basicConfig(level=logging.INFO)
-    # asyncio.run(serve())
     app.add_external_service(helloworld_service_pb2_grpc.add_HelloWorldServiceServicer_to_server, HelloWorldService())
     app.run(50051)
