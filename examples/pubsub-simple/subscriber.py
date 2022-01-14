f1511142e9e9bff8ab944b10b18baebf7a1c35ec
Renato L. de F. Cunha <renato.cunha@microsoft.com>
2022-01-14 10:51:55 -0300

    add TopicEventResponse example
    Signed-off-by: Renato L. de F. Cunha <renato.cunha@microsoft.com>
    Signed-off-by: GitHub <noreply@github.com>

--- examples/pubsub-simple/subscriber.py
+++ examples/pubsub-simple/subscriber.py
@@ -1,13 +1,15 @@
 from cloudevents.sdk.event import v1
 from dapr.ext.grpc import App
+from dapr.clients.grpc._response import TopicEventResponse
 
 import json
 
 app = App()
 
 @app.subscribe(pubsub_name='pubsub', topic='TOPIC_A')
-def mytopic(event: v1.Event) -> None:
+def mytopic(event: v1.Event) -> TopicEventResponse:
     data = json.loads(event.Data())
     print(f'Subscriber received: id={data["id"]}, message="{data["message"]}", content_type="{event.content_type}"',flush=True)
+    return TopicEventResponse('success')
 
 app.run(50051)
