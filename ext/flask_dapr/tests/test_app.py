a763405c9171bd82d875d8a168076fe0d066a342
Bernd Verst <4535280+berndverst@users.noreply.github.com>
2022-01-31 11:19:25 -0800

    Add PubSub subscriptions to Flask Dapr (#351)
    * Add PubSub subscriptions to Flask Dapr
    
    Signed-off-by: Bernd Verst <4535280+berndverst@users.noreply.github.com>
    Signed-off-by: GitHub <noreply@github.com>

---
+++ ext/flask_dapr/tests/test_app.py
@@ -0,0 +1,85 @@
+from flask import Flask
+from flask_dapr import DaprApp
+
+import unittest
+
+import json
+
+
+class DaprAppTest(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask("test_app")
+        self.app.testing = True
+        self.dapr_app = DaprApp(self.app)
+        self.client = self.app.test_client()
+
+    def test_subscribe_subscription_registered(self):
+        @self.dapr_app.subscribe(pubsub="pubsub", topic="test")
+        def event_handler():
+            return "default route"
+
+        self.assertEqual(len(self.dapr_app._subscriptions), 1)
+
+        self.assertIn("/dapr/subscribe", [r.rule for r in self.app.url_map.iter_rules()])
+        self.assertIn("/events/pubsub/test", [r.rule for r in self.app.url_map.iter_rules()])
+
+        response = self.client.get("/dapr/subscribe")
+        self.assertEqual(
+            [{'pubsubname': 'pubsub',
+                'topic': 'test',
+                'route': '/events/pubsub/test',
+                'metadata': {}
+              }], json.loads(response.data))
+
+        response = self.client.post("/events/pubsub/test", json={"body": "new message"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode("utf-8"), "default route")
+
+    def test_subscribe_with_route_subscription_registered_with_custom_route(self):
+        @self.dapr_app.subscribe(pubsub="pubsub", topic="test", route="/do-something")
+        def event_handler():
+            return "custom route"
+
+        self.assertEqual(len(self.dapr_app._subscriptions), 1)
+
+        self.assertIn("/dapr/subscribe", [r.rule for r in self.app.url_map.iter_rules()])
+        self.assertIn("/do-something", [r.rule for r in self.app.url_map.iter_rules()])
+
+        response = self.client.get("/dapr/subscribe")
+        self.assertEqual(
+            [{'pubsubname': 'pubsub',
+              'topic': 'test',
+              'route': '/do-something',
+              'metadata': {}
+              }], json.loads(response.data))
+
+        response = self.client.post("/do-something", json={"body": "new message"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode("utf-8"), 'custom route')
+
+    def test_subscribe_metadata(self):
+        handler_metadata = {"rawPayload": "true"}
+
+        @self.dapr_app.subscribe(pubsub="pubsub",
+                                 topic="test",
+                                 metadata=handler_metadata)
+        def event_handler():
+            return "custom metadata"
+
+        self.assertEqual((self.dapr_app._subscriptions[0]["metadata"]["rawPayload"]), "true")
+
+        response = self.client.get("/dapr/subscribe")
+        self.assertEqual(
+            [{'pubsubname': 'pubsub',
+              'topic': 'test',
+              'route': '/events/pubsub/test',
+              'metadata': {"rawPayload": "true"}
+              }], json.loads(response.data))
+
+        response = self.client.post("/events/pubsub/test", json={"body": "new message"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode("utf-8"), "custom metadata")
+
+
+if __name__ == '__main__':
+    unittest.main()
