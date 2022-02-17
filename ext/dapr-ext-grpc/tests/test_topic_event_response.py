13e973cc48b529e83dd74e6d1e1211e31d76e0d0
Renato Luiz de Freitas Cunha <renato.cunha@microsoft.com>
2022-02-17 19:41:36 +0000

    fix linting errors in test file
    Signed-off-by: Renato Luiz de Freitas Cunha <renato.cunha@microsoft.com>

--- ext/dapr-ext-grpc/tests/test_topic_event_response.py
+++ ext/dapr-ext-grpc/tests/test_topic_event_response.py
@@ -37,5 +37,6 @@ class TopicEventResponseTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             TopicEventResponse(None)
 
+
 if __name__ == '__main__':
     unittest.main()
