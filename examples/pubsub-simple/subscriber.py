e1c5c1a88380f8ef1147541247a7c3fcbc4c160c
Bernd Verst <4535280+berndverst@users.noreply.github.com>
2022-04-22 22:04:19 -0700

    Fix flakey pubsub integration test (#395)

--- examples/pubsub-simple/subscriber.py
+++ examples/pubsub-simple/subscriber.py
@@ -1,3 +1,17 @@
+# ------------------------------------------------------------
+# Copyright 2022 The Dapr Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------
+
+from time import sleep
 from cloudevents.sdk.event import v1
 from dapr.ext.grpc import App
 from dapr.clients.grpc._response import TopicEventResponse
@@ -8,24 +22,36 @@ import json
 app = App()
 should_retry = True  # To control whether dapr should retry sending a message
 
+
 @app.subscribe(pubsub_name='pubsub', topic='TOPIC_A')
 def mytopic(event: v1.Event) -> TopicEventResponse:
     global should_retry
     data = json.loads(event.Data())
-    print(f'Subscriber received: id={data["id"]}, message="{data["message"]}", content_type="{event.content_type}"',flush=True)
+    print(f'Subscriber received: id={data["id"]}, message="{data["message"]}", '
+          f'content_type="{event.content_type}"', flush=True)
     if should_retry:
         should_retry = False  # we only retry once in this example
+        sleep(0.5)  # add some delay to help with ordering of expected logs
         return TopicEventResponse('retry')
     return TopicEventResponse('success')
 
-# workaround as redis pubsub does not support wildcards
-for id in range(10):
-    app._servicer._registered_topics.append(appcallback_v1.TopicSubscription(pubsub_name='pubsub',topic=f'topic/{id}'))
 
-@app.subscribe(pubsub_name='pubsub', topic='topic/#', disable_topic_validation=True,)
+# == for testing with Redis only ==
+# workaround as redis pubsub does not support wildcards
+# we manually register the distinct topics
+for id in range(4, 7):
+    app._servicer._registered_topics.append(appcallback_v1.TopicSubscription(
+        pubsub_name='pubsub', topic=f'topic/{id}'))
+# =================================
+
+
+# this allows subscribing to all events sent to this app - useful for wildcard topics
+@app.subscribe(pubsub_name='pubsub', topic='topic/#', disable_topic_validation=True)
 def mytopic_wildcard(event: v1.Event) -> TopicEventResponse:
     data = json.loads(event.Data())
-    print(f'Wildcard-Subscriber received: id={data["id"]}, message="{data["message"]}", content_type="{event.content_type}"',flush=True)
+    print(f'Wildcard-Subscriber received: id={data["id"]}, message="{data["message"]}", '
+          f'content_type="{event.content_type}"', flush=True)
     return TopicEventResponse('success')
+
 
 app.run(50051)
