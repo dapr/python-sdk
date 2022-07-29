3df74309d3ebd259f3f12cbb91e065d15bfe776c
Tiago Alves Macambira <tmacam@burocrata.org>
2022-07-28 19:34:25 -0700

    Addressing PR comments (cont'ed.)
    Signed-off-by: Tiago Alves Macambira <tmacam@burocrata.org>

---
+++ examples/distributed_lock/lock.py
@@ -0,0 +1,44 @@
+# ------------------------------------------------------------
+# Copyright 2021 The Dapr Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------
+
+from dapr.clients import DaprClient
+import warnings
+
+
+def main():
+    # Lock parameters
+    store_name = 'lockstore'  # as defined in components/lockstore.yaml
+    resource_id = 'example-lock-resource'
+    client_id = 'example-client-id'
+    expiry_in_seconds = 60
+
+    with DaprClient() as dapr:
+        print('Will try to acquire a lock from lock store named [%s]' % store_name)
+        print('The lock is for a resource named [%s]' % resource_id)
+        print('The client identifier is [%s]' % client_id)
+        print('The lock will will expire in %s seconds.' % expiry_in_seconds)
+
+        with dapr.try_lock(store_name, resource_id, client_id, expiry_in_seconds) as lock_result:
+            assert(lock_result.success)
+            print('Lock acquired successfully!!!')
+
+        # At this point the lock was released - by magic of the `with` clause ;)
+        unlock_result = dapr.unlock(store_name, resource_id, client_id)
+        print('We already released the lock so unlocking will not work.')
+        print('We tried to unlock it anyway and got back [%s]' % unlock_result.status)
+
+
+if __name__ == '__main__':
+    # Suppress "The Distributed Lock API is an Alpha" warnings
+    warnings.simplefilter("ignore")
+    main()
