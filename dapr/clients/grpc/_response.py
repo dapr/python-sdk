00a8381e3e67a12f1ba53306f06c3bc2b36334ea
Bernd Verst <github@bernd.dev>
2023-04-17 15:34:09 -0700

    fix type check error
    Signed-off-by: Bernd Verst <github@bernd.dev>

--- dapr/clients/grpc/_response.py
+++ dapr/clients/grpc/_response.py
@@ -861,8 +861,8 @@ class TryLockResponse(contextlib.AbstractContextManager, DaprResponse):
         if unlock doesn't return `UnlockResponseStatus.success`.
         '''
         if self._success:
-            await self._client.unlock(self._store_name,
-                                      self._resource_id, self._lock_owner)  # type: ignore
+            await self._client.unlock(self._store_name,   # type: ignore
+                                      self._resource_id, self._lock_owner)
         # else: there is no point unlocking a lock we did not acquire.
 
     async def __aenter__(self) -> 'TryLockResponse':
