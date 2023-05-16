bf69dea4067c4c9f4dec0f4d007367cc0a600c3b
Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>
2023-05-16 09:22:47 +0530

    Include raise event test and assertions
    Signed-off-by: Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>

--- examples/demo_workflow/app.py
+++ examples/demo_workflow/app.py
@@ -10,6 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from time import sleep
 from dapr.ext.workflow.workflow_runtime import WorkflowRuntime
 from dapr.ext.workflow.dapr_workflow_client import DaprWorkflowClient
 from dapr.ext.workflow.dapr_workflow_context import DaprWorkflowContext
@@ -19,20 +20,25 @@ from dapr.conf import Settings
 
 settings = Settings()
 
+counter = 0
+
 def hello_world_wf(ctx: DaprWorkflowContext, input):
     print(f'{input}')
-    yield ctx.call_activity(hello_act, input='  Boil one cup of water. ')
-    yield ctx.call_activity(hello_act, input='  Add tea leaves to the cup. ')
-    yield ctx.call_activity(hello_act, input='  Add boiled water to the cup. ')
-    yield ctx.call_activity(hello_act, input='  Add sugar and milk to the cup. ')
+    yield ctx.call_activity(hello_act, input=1)
+    yield ctx.call_activity(hello_act, input=10)
+    yield ctx.wait_for_external_event("event1")
+    yield ctx.call_activity(hello_act, input=100)
+    yield ctx.call_activity(hello_act, input=1000)
 
 def hello_act(ctx: WorkflowActivityContext, input):
-    print(f'{input}!', flush=True)
+    global counter
+    counter += input
+    print(f'New counter value is: {counter}!', flush=True)
 
-daprRuntime = WorkflowRuntime()
-daprRuntime.register_workflow(hello_world_wf)
-daprRuntime.register_activity(hello_act)
-daprRuntime.start()
+daprOptions = WorkflowRuntime()
+daprOptions.register_workflow(hello_world_wf)
+daprOptions.register_activity(hello_act)
+daprOptions.start()
 
 host = settings.DAPR_RUNTIME_HOST
 if host is None:
@@ -42,8 +48,16 @@ if port is None:
     port = "4001"
 
 client = DaprWorkflowClient(host, port)
-print("==========Steps to prepare tea:==========")
+print("==========Start Counter Increase as per Input:==========")
 id = client.schedule_new_workflow(hello_world_wf, input='Hi Chef!')
-status = client.wait_for_workflow_completion(id, timeout=30)
-print("  Preparation of tea is ", status.runtime_status.name)
-daprRuntime.shutdown()
+# Sleep for a while to let the workflow run
+sleep(1)
+assert counter == 11
+sleep(10)
+client.raise_workflow_event(id, "event1")
+# Sleep for a while to let the workflow run
+sleep(1)
+assert counter == 1111
+status = client.wait_for_workflow_completion(id, timeout=6000)
+assert status.runtime_status.name == "COMPLETED"
+daprOptions.shutdown()
