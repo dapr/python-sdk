efd842040af833aeb36ae7c5d7cb01365c30397e
Ryan Lettieri <ryanLettieri@microsoft.com>
2023-05-30 09:04:24 -0600

    Code formatting for workflow
    Signed-off-by: Ryan Lettieri <ryanLettieri@microsoft.com>

--- dapr/aio/clients/grpc/client.py
+++ dapr/aio/clients/grpc/client.py
@@ -1149,14 +1149,12 @@ class DaprGrpcClientAsync:
             encoded_data = input
         else:
             try:
-                if input is not None:
-                    encoded_data = json.dumps(input).encode("utf-8")
-                else:
-                    encoded_data = bytes([])
+                encoded_data = json.dumps(input).encode(
+                    "utf-8") if input is not None else bytes([])
             except TypeError:
-                raise DaprInternalError("Json Data is not serializable")
+                raise DaprInternalError("start_workflow: input data must be JSON serializable")
             except ValueError as e:
-                raise DaprInternalError(f"Json serialization error: {e}")
+                raise DaprInternalError(f"start_workflow JSON serialization error: {e}")
 
         # Actual start workflow invocation
         req = api_v1.StartWorkflowRequest(
@@ -1250,7 +1248,7 @@ class DaprGrpcClientAsync:
             instance_id: str,
             workflow_component: str,
             event_name: str,
-            event_data: Union[Any, bytes, None] = None,
+            event_data: Optional[Union[Any, bytes]] = None,
             send_raw_bytes: bool = False) -> DaprResponse:
         """Raises an event on a workflow.
 
@@ -1261,7 +1259,7 @@ class DaprGrpcClientAsync:
                                     that will run the workflow. e.g. `dapr`.
                 event_name (str): the name of the event to be raised on
                                     the workflow.
-                event_data (Union[Any, bytes, None]): the input to the event.
+                event_data (Optional[Union[Any, bytes]]): the input to the event.
                 send_raw_bytes (bool) if true, no serialization will be performed on the input
                                     bytes
 
@@ -1279,14 +1277,13 @@ class DaprGrpcClientAsync:
         else:
             if event_data is not None:
                 try:
-                    if event_data is not None:
-                        encoded_data = json.dumps(event_data).encode("utf-8")
-                    else:
-                        event_data = bytes([])
+                    encoded_data = json.dumps(event_data).encode(
+                        "utf-8") if event_data is not None else bytes([])
                 except TypeError:
-                    raise DaprInternalError("Json Data is not serializable")
+                    raise DaprInternalError("raise_workflow_event:\
+                                             event_data must be JSON serializable")
                 except ValueError as e:
-                    raise DaprInternalError(f"Json serialization error: {e}")
+                    raise DaprInternalError(f"raise_workflow_event JSON serialization error: {e}")
                 encoded_data = json.dumps(event_data).encode("utf-8")
             else:
                 encoded_data = bytes([])
--- dapr/clients/grpc/client.py
+++ dapr/clients/grpc/client.py
@@ -1133,14 +1133,12 @@ class DaprGrpcClient:
             encoded_data = input
         else:
             try:
-                if input is not None:
-                    encoded_data = json.dumps(input).encode("utf-8")
-                else:
-                    encoded_data = bytes([])
+                encoded_data = json.dumps(input).encode(
+                    "utf-8") if input is not None else bytes([])
             except TypeError:
-                raise DaprInternalError("Json Data is not serializable")
+                raise DaprInternalError("start_workflow: input data must be JSON serializable")
             except ValueError as e:
-                raise DaprInternalError(f"Json serialization error: {e}")
+                raise DaprInternalError(f"start_workflow JSON serialization error: {e}")
 
         # Actual start workflow invocation
         req = api_v1.StartWorkflowRequest(
@@ -1234,7 +1232,7 @@ class DaprGrpcClient:
             instance_id: str,
             workflow_component: str,
             event_name: str,
-            event_data: Union[Any, bytes, None] = None,
+            event_data: Optional[Union[Any, bytes]] = None,
             send_raw_bytes: bool = False) -> DaprResponse:
         """Raises an event on a workflow.
 
@@ -1245,7 +1243,7 @@ class DaprGrpcClient:
                                     that will run the workflow. e.g. `dapr`.
                 event_name (str): the name of the event to be raised on
                                     the workflow.
-                event_data (Union[Any, bytes, None]): the input to the event.
+                event_data (Optional[Union[Any, bytes]]): the input to the event.
                 send_raw_bytes (bool) if true, no serialization will be performed on the input
                                     bytes
 
@@ -1264,14 +1262,13 @@ class DaprGrpcClient:
         else:
             if event_data is not None:
                 try:
-                    if event_data is not None:
-                        encoded_data = json.dumps(event_data).encode("utf-8")
-                    else:
-                        event_data = bytes([])
+                    encoded_data = json.dumps(event_data).encode(
+                        "utf-8") if event_data is not None else bytes([])
                 except TypeError:
-                    raise DaprInternalError("Json Data is not serializable")
+                    raise DaprInternalError("raise_workflow_event:\
+                                             event_data must be JSON serializable")
                 except ValueError as e:
-                    raise DaprInternalError(f"Json serialization error: {e}")
+                    raise DaprInternalError(f"raise_workflow_event JSON serialization error: {e}")
                 encoded_data = json.dumps(event_data).encode("utf-8")
             else:
                 encoded_data = bytes([])
