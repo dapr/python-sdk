6e0fbbb1543356d10a4d51c8c21111d02042b2e1
Elena Kolevska <elena@kolevska.com>
2023-08-17 12:39:09 +0100

    Adds unit tests for endpointparsing
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- tests/clients/test_dapr_async_grpc_client.py
+++ tests/clients/test_dapr_async_grpc_client.py
@@ -230,6 +230,28 @@ class DaprGrpcClientAsyncTests(unittest.IsolatedAsyncioTestCase):
         self.assertEqual({}, resp.binding_metadata)
         self.assertEqual(0, len(resp.headers))
 
+    def test_parse_endpoint(self):
+        testcases = [
+            {"endpoint": "http://domain.com", "scheme": "http", "host": "domain.com", "port": 80},
+            {"endpoint": "http://domain.com:5000", "scheme": "http", "host": "domain.com", "port": 5000},
+
+            {"endpoint": "https://domain.com", "scheme": "https", "host": "domain.com", "port": 443},
+            {"endpoint": "https://domain.com:5000", "scheme": "https", "host": "domain.com", "port": 5000},
+
+            {"endpoint": "http://domain.com/v1/dapr", "scheme": "http", "host": "domain.com", "port": 80},
+            {"endpoint": "https://domain.com/v1/dapr", "scheme": "https", "host": "domain.com", "port": 443},
+
+            {"endpoint": "http://domain.com:5000/v1/dapr", "scheme": "http", "host": "domain.com", "port": 5000},
+            {"endpoint": "https://domain.com:5000/v1/dapr", "scheme": "https", "host": "domain.com", "port": 5000},
+        ]
+        for testcase in testcases:
+            dapr = DaprGrpcClientAsync()
+            dapr._parse_endpoint(testcase["endpoint"])
+
+            self.assertEqual(testcase["scheme"], dapr._scheme)
+            self.assertEqual(testcase["host"], dapr._hostname)
+            self.assertEqual(testcase["port"], dapr._port)
+
     async def test_publish_event(self):
         dapr = DaprGrpcClientAsync(f'localhost:{self.server_port}')
         resp = await dapr.publish_event(
