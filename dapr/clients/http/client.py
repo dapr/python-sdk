7674566f83761b4baeea96a849a4a49fcc4ff480
Elena Kolevska <elena@kolevska.com>
2023-08-20 18:36:02 +0100

    Fixes http invocation. Adds examples for http invocation.
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- dapr/clients/http/client.py
+++ dapr/clients/http/client.py
@@ -17,6 +17,8 @@ import aiohttp
 
 from typing import Callable, Mapping, Dict, Optional, Union, Tuple, TYPE_CHECKING
 
+from dapr.clients.grpc._helpers import parse_endpoint
+
 if TYPE_CHECKING:
     from dapr.serializers import Serializer
 
@@ -50,10 +52,14 @@ class DaprHttpClient:
         self._headers_callback = headers_callback
 
     def get_api_url(self) -> str:
-        return settings.DAPR_HTTP_ENDPOINT or 'http://{}:{}/{}'.format(
-                settings.DAPR_RUNTIME_HOST,
-                settings.DAPR_HTTP_PORT,
-                settings.DAPR_API_VERSION)
+        if settings.DAPR_HTTP_ENDPOINT:
+            scheme, host, port = parse_endpoint(settings.DAPR_HTTP_ENDPOINT)
+        else:
+            scheme = 'http'
+            host = settings.DAPR_RUNTIME_HOST
+            port = settings.DAPR_HTTP_PORT
+
+        return f'{scheme}://{host}:{port}/{settings.DAPR_API_VERSION}'
 
     async def send_bytes(
             self, method: str, url: str,
---
+++ examples/invoke-http/invoke-caller.py
@@ -0,0 +1,26 @@
+import json
+import time
+
+from dapr.clients import DaprClient
+
+with DaprClient() as d:
+    req_data = {
+        'id': 1,
+        'message': 'hello world'
+    }
+
+    while True:
+        # Create a typed message with content type and body
+        resp = d.invoke_method(
+            'invoke-receiver',
+            'my-method',
+            http_verb='POST',
+            data=json.dumps(req_data),
+        )
+
+        # Print the response
+        print(resp.content_type, flush=True)
+        print(resp.text(), flush=True)
+        print(str(resp.status_code), flush=True)
+
+        time.sleep(2)
