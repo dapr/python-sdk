bc7c88082254e9a0716d64c8da08f57f48f242c1
Elena Kolevska <elena@kolevska.com>
2023-10-13 01:04:01 +0100

    Adds support for unix URIs. Clean up.
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- dapr/conf/helpers.py
+++ dapr/conf/helpers.py
@@ -1,59 +1,65 @@
 from urllib.parse import urlparse, parse_qs
+from typing import Optional
 
-DEFAULT_SCHEME = "dns"
-DEFAULT_HOSTNAME = "localhost"
-DEFAULT_PORT = 443
-DEFAULT_PATH = ""
-DEFAULT_QUERY = ""
-DEFAULT_TLS_PORT = 443
-DEFAULT_TLS = False
-ACCEPTED_SCHEMES = ["dns", "unix", "unix-abstract", "vsock", "http", "https", "grpc", "grpcs"]
-VALID_SCHEMES = ["dns", "unix", "unix-abstract", "vsock", "grpc", "grpcs"]
+
+class URIParseConfig:
+    DEFAULT_SCHEME = "dns"
+    DEFAULT_HOSTNAME = "localhost"
+    DEFAULT_PORT = 443
+    DEFAULT_TLS = False
+    ACCEPTED_SCHEMES = ["dns", "unix", "unix-abstract", "vsock", "http", "https", "grpc", "grpcs"]
+    VALID_SCHEMES = ["dns", "unix", "unix-abstract", "vsock", "grpc", "grpcs"]
 
 
 class Endpoint:
-    scheme: str
-    hostname: str
-    port: int
-    path: str
-    query: str
-    tls: bool
-
-    def __init__(self, scheme: str, hostname: str, port: int, path: str, query: str, tls: bool):
-        self.scheme = scheme or DEFAULT_SCHEME
-        self.hostname = hostname or DEFAULT_HOSTNAME
-        self.port = port or DEFAULT_PORT
-        self.path = path or DEFAULT_PATH
-        self.query = query or DEFAULT_QUERY
-        self.tls = tls or DEFAULT_TLS
+    def __init__(self, scheme: str, hostname: str, port: Optional[int], tls: bool):
+        self.scheme = scheme or URIParseConfig.DEFAULT_SCHEME
+        self.hostname = hostname or URIParseConfig.DEFAULT_HOSTNAME
+        self.port = port or URIParseConfig.DEFAULT_PORT
+        self.tls = tls or URIParseConfig.DEFAULT_TLS
 
     def is_secure(self) -> bool:
         return self.tls
 
     def get_scheme(self) -> str:
-        return self.scheme if self.scheme in VALID_SCHEMES else DEFAULT_SCHEME
+        return self.scheme if self.scheme in URIParseConfig.VALID_SCHEMES else URIParseConfig.DEFAULT_SCHEME
 
     def get_port(self) -> str:
+        if self.scheme in ["unix", "unix-abstract", "vsock"]:
+            return ""
+
         return str(self.port)
 
     def get_hostname(self) -> str:
         hostname = self.hostname
-        hostname_list = hostname.split(":")
-        if len(hostname_list) == 8:
+        if self.hostname.count(":") == 7:
             # IPv6 address
             hostname = f"[{hostname}]"
         return hostname
 
     def get_endpoint(self) -> str:
-        return f"{self.get_scheme()}:{self.get_hostname()}:{self.get_port()}"
+        scheme = self.get_scheme()
+        if scheme in ["unix", "unix-abstract", "vsock"]:
+            return f"{scheme}://{self.hostname}"
+
+        return f"{scheme}:{self.get_hostname()}:{self.get_port()}"
 
 
 def parse_grpc_endpoint(url: str) -> Endpoint:
+    url = preprocess_url(url)
+    parsed_url = urlparse(url)
+    validate_path_and_query(parsed_url.path, parsed_url.query, parsed_url.scheme)
+    tls = extract_tls_from_query(parsed_url.query, parsed_url.scheme)
+
+    return Endpoint(parsed_url.scheme, parsed_url.hostname, parsed_url.port, tls)
+
+
+def preprocess_url(url: str) -> str:
     url_list = url.split(":")
     if len(url_list) == 3 and "://" not in url:
         # A URI like dns:mydomain:5000 was used
         url = url.replace(":", "://", 1)
-    elif len(url_list) == 2 and "://" not in url and url_list[0] in ACCEPTED_SCHEMES:
+    elif len(url_list) == 2 and "://" not in url and url_list[0] in URIParseConfig.ACCEPTED_SCHEMES:
         # A URI like dns:mydomain was used
         url = url.replace(":", "://", 1)
     else:
@@ -62,37 +68,27 @@ def parse_grpc_endpoint(url: str) -> Endpoint:
             # If a scheme was not explicitly specified in the URL
             # we need to add a default scheme,
             # because of how urlparse works
-            url = f'{DEFAULT_SCHEME}://{url}'
+            url = f'{URIParseConfig.DEFAULT_SCHEME}://{url}'
+    return url
 
-    parsed_url = urlparse(url)
-    scheme = parsed_url.scheme
-    hostname = parsed_url.hostname or DEFAULT_HOSTNAME
-    port = parsed_url.port
-    path = parsed_url.path
-    query = parsed_url.query
 
-    if len(path) > 0:
-        raise ValueError(f"paths are not supported for gRPC endpoints: '{path}' in '{url}'")
+def validate_path_and_query(path: str, query: str, scheme: str) -> None:
+    if path:
+        raise ValueError(f"Paths are not supported for gRPC endpoints: '{path}'")
+    if query:
+        query_dict = parse_qs(query)
+        if 'tls' in query_dict and scheme in ["http", "https"]:
+            raise ValueError(
+                f"The tls query parameter is not supported for http(s) endpoints: '{query}'")
+        query_dict.pop('tls', None)
+        if query_dict:
+            raise ValueError(f"Query parameters are not supported for gRPC endpoints: '{query}'")
 
+
+def extract_tls_from_query(query: str, scheme: str) -> bool:
     query_dict = parse_qs(query)
-
-    # Check if the query string contains a tls parameter
-    tls = False
-    if 'tls' in query_dict and scheme in ["http", "https"]:
-        raise ValueError(
-            f"the tls query parameter is not supported for http(s) endpoints: '{query}' in '{url}'")
-    if 'tls' in query_dict:
-        tls = query_dict.get('tls')[0].lower() == "true"
-
-    # Special case for backwards compatibility
+    tls = query_dict.get('tls', [None])[0]
+    tls = tls and tls.lower() == 'true'
     if scheme == "https":
         tls = True
-        scheme = DEFAULT_SCHEME
-        port = port or DEFAULT_TLS_PORT
-
-    query_dict.pop('tls', None)
-    if len(query_dict) > 0:
-        raise ValueError(
-            f"query parameters are not supported for gRPC endpoints: '{query}' in '{url}'")
-
-    return Endpoint(scheme, hostname, port, path, query, tls)
+    return tls
--- tests/conf/helpers_test.py
+++ tests/conf/helpers_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from dapr.conf.helpers import parse_endpoint, parse_grpc_endpoint
+from dapr.conf.helpers import parse_grpc_endpoint
 
 
 class DaprClientHelpersTests(unittest.TestCase):
@@ -8,28 +8,28 @@ class DaprClientHelpersTests(unittest.TestCase):
     def test_parse_grpc_endpoint(self):
         testcases = [
             # Port only
-            # {"url": ":5000", "error": False, "secure": False, "scheme": "", "host": "localhost",
-            #  "port": 5000, "endpoint": "dns:localhost:5000"},
-            # {"url": ":5000?tls=false", "error": False, "secure": False, "scheme": "",
-            #  "host": "localhost", "port": 5000, "endpoint": "dns:localhost:5000"},
-            # {"url": ":5000?tls=true", "error": False, "secure": True, "scheme": "",
-            #  "host": "localhost", "port": 5000, "endpoint": "dns:localhost:5000"},
-            #
-            # # Host only
-            # {"url": "myhost", "error": False, "secure": False, "scheme": "", "host": "myhost",
-            #  "port": 443, "endpoint": "dns:myhost:443"},
-            # {"url": "myhost?tls=false", "error": False, "secure": False, "scheme": "",
-            #  "host": "myhost", "port": 443, "endpoint": "dns:myhost:443"},
-            # {"url": "myhost?tls=true", "error": False, "secure": True, "scheme": "",
-            #  "host": "myhost", "port": 443, "endpoint": "dns:myhost:443"},
-            #
-            # # Host and port
-            # {"url": "myhost:443", "error": False, "secure": False, "scheme": "", "host": "myhost",
-            #  "port": 443, "endpoint": "dns:myhost:443"},
-            # {"url": "myhost:443?tls=false", "error": False, "secure": False, "scheme": "",
-            #  "host": "myhost", "port": 443, "endpoint": "dns:myhost:443"},
-            # {"url": "myhost:443?tls=true", "error": False, "secure": True, "scheme": "",
-            #  "host": "myhost", "port": 443, "endpoint": "dns:myhost:443"},
+            {"url": ":5000", "error": False, "secure": False, "scheme": "", "host": "localhost",
+             "port": 5000, "endpoint": "dns:localhost:5000"},
+            {"url": ":5000?tls=false", "error": False, "secure": False, "scheme": "",
+             "host": "localhost", "port": 5000, "endpoint": "dns:localhost:5000"},
+            {"url": ":5000?tls=true", "error": False, "secure": True, "scheme": "",
+             "host": "localhost", "port": 5000, "endpoint": "dns:localhost:5000"},
+
+            # Host only
+            {"url": "myhost", "error": False, "secure": False, "scheme": "", "host": "myhost",
+             "port": 443, "endpoint": "dns:myhost:443"},
+            {"url": "myhost?tls=false", "error": False, "secure": False, "scheme": "",
+             "host": "myhost", "port": 443, "endpoint": "dns:myhost:443"},
+            {"url": "myhost?tls=true", "error": False, "secure": True, "scheme": "",
+             "host": "myhost", "port": 443, "endpoint": "dns:myhost:443"},
+
+            # Host and port
+            {"url": "myhost:443", "error": False, "secure": False, "scheme": "", "host": "myhost",
+             "port": 443, "endpoint": "dns:myhost:443"},
+            {"url": "myhost:443?tls=false", "error": False, "secure": False, "scheme": "",
+             "host": "myhost", "port": 443, "endpoint": "dns:myhost:443"},
+            {"url": "myhost:443?tls=true", "error": False, "secure": True, "scheme": "",
+             "host": "myhost", "port": 443, "endpoint": "dns:myhost:443"},
 
             # Scheme, host and port
             {"url": "http://myhost", "error": False, "secure": False, "scheme": "",
@@ -73,19 +73,34 @@ class DaprClientHelpersTests(unittest.TestCase):
             {"url": "dns://myhost?tls=true", "error": False, "secure": True, "scheme": "dns",
              "host": "myhost", "port": 443, "endpoint": "dns:myhost:443"},
 
+            # Unix sockets
+            {"url": "unix://my.sock", "error": False, "secure": False, "scheme": "unix",
+             "host": "my.sock", "port": "", "endpoint": "unix://my.sock"},
+            {"url": "unix://my.sock?tls=true", "error": False, "secure": True, "scheme": "unix",
+             "host": "my.sock", "port": "", "endpoint": "unix://my.sock"},
+
+
             # IPv6 addresses
             {"url": "[2001:db8:1f70::999:de8:7648:6e8]", "error": False, "secure": False, "scheme": "", "host":"[2001:db8:1f70::999:de8:7648:6e8]", "port": 443, "endpoint": "dns:[2001:db8:1f70::999:de8:7648:6e8]:443"},
             {"url": "dns:[2001:db8:1f70::999:de8:7648:6e8]", "error": False, "secure": False, "scheme": "", "host":"[2001:db8:1f70::999:de8:7648:6e8]", "port": 443, "endpoint": "dns:[2001:db8:1f70::999:de8:7648:6e8]:443"},
             {"url": "https://[2001:db8:1f70::999:de8:7648:6e8]", "error": False, "secure": True, "scheme": "", "host":"[2001:db8:1f70::999:de8:7648:6e8]", "port": 443, "endpoint": "dns:[2001:db8:1f70::999:de8:7648:6e8]:443"},
 
-            {"url": "myhost:5000/v1/dapr", "error": True},
-            # Paths are not allowed in grpc endpoints
-            {"url": "myhost:5000/?a=1", "error": True},
-            # Query parameters are not allowed in grpc endpoints
+            # Invalid addresses (with path and queries)
+            {"url": "host:5000/v1/dapr", "error": True},  # Paths are not allowed in grpc endpoints
+            {"url": "host:5000/?a=1", "error": True},  # Query params not allowed in grpc endpoints
         ]
 
-        print(f'URL \t endpoint\t  hostname \t port \t secure \t error')
         for testcase in testcases:
+            try:
+                endpoint = parse_grpc_endpoint(testcase["url"])
+                print(f'{testcase["url"]}\t {endpoint.get_endpoint()} \t{endpoint.get_hostname()}\t{endpoint.get_port()}\t{endpoint.is_secure()}')
+                assert endpoint.get_endpoint() == testcase["endpoint"]
+                assert endpoint.is_secure() == testcase["secure"]
+                assert endpoint.get_hostname() == testcase["host"]
+                assert endpoint.get_port() == str(testcase["port"])
+            except ValueError as error:
+                print(f'{testcase["url"]}\t {error}')
+
             if testcase["error"]:
                 with self.assertRaises(ValueError):
                     parse_grpc_endpoint(testcase["url"])
@@ -93,7 +108,7 @@ class DaprClientHelpersTests(unittest.TestCase):
                 endpoint = parse_grpc_endpoint(testcase["url"])
                 print(
                     f'{testcase["url"]}\t {endpoint.get_endpoint()} \t{endpoint.get_hostname()}\t{endpoint.get_port()}\t{endpoint.is_secure()}')
-                assert testcase["endpoint"] == endpoint.get_endpoint()
-                assert testcase["secure"] == endpoint.is_secure()
-                assert testcase["host"] == endpoint.get_hostname()
-                assert str(testcase["port"]) == endpoint.get_port()
+                assert endpoint.get_endpoint() == testcase["endpoint"]
+                assert endpoint.is_secure() == testcase["secure"]
+                assert endpoint.get_hostname() == testcase["host"]
+                assert endpoint.get_port() == str(testcase["port"])
