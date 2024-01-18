9bf91ca3ad0eedc78acc3c39b6abaaf60b93763f
Li Tan <litan1106@gmail.com>
2024-01-18 14:00:42 -0800

    Add ruff
    Signed-off-by: Bernd Verst <github@bernd.dev>

--- setup.py
+++ setup.py
@@ -19,19 +19,19 @@ from setuptools import setup
 
 # Load version in dapr package.
 version_info = {}
-with open('dapr/version/version.py') as fp:
+with open("dapr/version/version.py") as fp:
     exec(fp.read(), version_info)
-__version__ = version_info['__version__']
+__version__ = version_info["__version__"]
 
 
 def is_release():
-    return '.dev' not in __version__
+    return ".dev" not in __version__
 
 
-name = 'dapr'
+name = "dapr"
 version = __version__
-description = 'The official release of Dapr Python SDK.'
-long_description = '''
+description = "The official release of Dapr Python SDK."
+long_description = """
 Dapr is a portable, serverless, event-driven runtime that makes it easy for developers to
 build resilient, stateless and stateful microservices that run on the cloud and edge and
 embraces the diversity of languages and developer frameworks.
@@ -40,18 +40,18 @@ Dapr codifies the best practices for building microservice applications into ope
 independent, building blocks that enable you to build portable applications with the language
 and framework of your choice. Each building block is independent and you can use one, some,
 or all of them in your application.
-'''.lstrip()
+""".lstrip()
 
 # Get build number from GITHUB_RUN_NUMBER environment variable
-build_number = os.environ.get('GITHUB_RUN_NUMBER', '0')
+build_number = os.environ.get("GITHUB_RUN_NUMBER", "0")
 
 if not is_release():
-    name += '-dev'
-    version = f'{__version__}{build_number}'
-    description = 'The developmental release for Dapr Python SDK.'
-    long_description = 'This is the developmental release for Dapr Python SDK.'
+    name += "-dev"
+    version = f"{__version__}{build_number}"
+    description = "The developmental release for Dapr Python SDK."
+    long_description = "This is the developmental release for Dapr Python SDK."
 
-print(f'package name: {name}, version: {version}', flush=True)
+print(f"package name: {name}, version: {version}", flush=True)
 
 
 setup(
