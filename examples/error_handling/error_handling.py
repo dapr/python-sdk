100e491bef17622b4c23baba0aa5d018025d80de
Elena Kolevska <elena@kolevska.com>
2024-01-23 19:32:23 +0000

    Runs ruff
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- examples/error_handling/error_handling.py
+++ examples/error_handling/error_handling.py
@@ -1,4 +1,3 @@
-
 from dapr.clients import DaprClient
 from dapr.clients.exceptions import DaprGrpcError
 
@@ -16,10 +15,10 @@ with DaprClient() as d:
         d.save_state(store_name=storeName, key=key, value=value)
     except DaprGrpcError as err:
         print(f'Status code: {err.code()}')
-        print(f"Message: {err.message()}")
-        print(f"Error code: {err.error_code()}")
-        print(f"Error info(reason): {err.error_info.reason}")
-        print(f"Resource info (resource type): {err.resource_info.resource_type}")
-        print(f"Resource info (resource name): {err.resource_info.resource_name}")
-        print(f"Bad request (field): {err.bad_request.field_violations[0].field}")
-        print(f"Bad request (description): {err.bad_request.field_violations[0].description}")
+        print(f'Message: {err.message()}')
+        print(f'Error code: {err.error_code()}')
+        print(f'Error info(reason): {err.error_info.reason}')
+        print(f'Resource info (resource type): {err.resource_info.resource_type}')
+        print(f'Resource info (resource name): {err.resource_info.resource_name}')
+        print(f'Bad request (field): {err.bad_request.field_violations[0].field}')
+        print(f'Bad request (description): {err.bad_request.field_violations[0].description}')
