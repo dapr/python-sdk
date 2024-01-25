781e2f29e855931ae60dacf4cc6609326ad56ac4
Bernd Verst <github@bernd.dev>
2024-01-24 16:15:11 -0800

    Update tracing example for opentelemetry (#664)
    * Update tracing example for opentelemetry
    
    Signed-off-by: Bernd Verst <github@bernd.dev>
    
    * autoformat
    
    Signed-off-by: Bernd Verst <github@bernd.dev>
    
    ---------
    
    Signed-off-by: Bernd Verst <github@bernd.dev>

--- examples/w3c-tracing/invoke-receiver.py
+++ examples/w3c-tracing/invoke-receiver.py
@@ -1,41 +1,88 @@
+import json
 import time
-
+import typing
 from concurrent import futures
 
+from opentelemetry import trace
+from opentelemetry.exporter.zipkin.json import ZipkinExporter
+from opentelemetry.instrumentation.grpc import GrpcInstrumentorServer, filters
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.sampling import ALWAYS_ON
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
+
+from dapr.clients import DaprClient
 from dapr.ext.grpc import App, InvokeMethodRequest, InvokeMethodResponse
 
-from opencensus.trace.samplers import AlwaysOnSampler
-from opencensus.trace.tracer import Tracer
-from opencensus.ext.grpc import server_interceptor
-from opencensus.trace.samplers import AlwaysOnSampler
+# Create a tracer provider
+tracer_provider = TracerProvider(sampler=ALWAYS_ON)
 
-tracer_interceptor = server_interceptor.OpenCensusServerInterceptor(AlwaysOnSampler())
-app = App(
-    thread_pool=futures.ThreadPoolExecutor(max_workers=10), interceptors=(tracer_interceptor,)
-)
+# Create a span processor
+span_processor = BatchSpanProcessor(ZipkinExporter(endpoint='http://localhost:9411/api/v2/spans'))
+
+# Add the span processor to the tracer provider
+tracer_provider.add_span_processor(span_processor)
+
+# Set the tracer provider
+trace.set_tracer_provider(tracer_provider)
+
+# Get the tracer
+tracer = trace.get_tracer(__name__)
+
+# automatically intercept incoming tracing headers and propagate them to the current context
+grpc_server_instrumentor = GrpcInstrumentorServer()
+grpc_server_instrumentor.instrument()
 
 
-@app.method(name='say')
-def say(request: InvokeMethodRequest) -> InvokeMethodResponse:
-    tracer = Tracer(sampler=AlwaysOnSampler())
-    with tracer.span(name='say') as span:
+app = App(thread_pool=futures.ThreadPoolExecutor(max_workers=10))
+
+
+@app.method(name='saytrace')
+def saytrace(request: InvokeMethodRequest) -> InvokeMethodResponse:
+    with tracer.start_as_current_span(name='say') as span:
         data = request.text()
-        span.add_annotation('Request length', len=len(data))
+        span.add_event(name='log', attributes={'Request length': len(data)})
         print(request.metadata, flush=True)
         print(request.text(), flush=True)
 
-        return InvokeMethodResponse(b'SAY', 'text/plain; charset=UTF-8')
+        resp = {'receivedtraceid': span.get_span_context().trace_id, 'method': 'SAY'}
+
+        return InvokeMethodResponse(json.dumps(resp), 'application/json; charset=UTF-8')
 
 
 @app.method(name='sleep')
 def sleep(request: InvokeMethodRequest) -> InvokeMethodResponse:
-    tracer = Tracer(sampler=AlwaysOnSampler())
-    with tracer.span(name='sleep') as _:
+    with tracer.start_as_current_span(name='sleep'):
         time.sleep(2)
         print(request.metadata, flush=True)
         print(request.text(), flush=True)
 
         return InvokeMethodResponse(b'SLEEP', 'text/plain; charset=UTF-8')
+
+
+# This method is used to forward the request to another service
+# this is used to test the tracing propagation
+@app.method(name='forward')
+def forward(request: InvokeMethodRequest) -> InvokeMethodResponse:
+    with tracer.start_as_current_span(name='forward'):
+        print(request.metadata, flush=True)
+        print(request.text(), flush=True)
+
+        # this helper method can be used to inject the tracing headers into the request
+        def trace_injector() -> typing.Dict[str, str]:
+            headers: typing.Dict[str, str] = {}
+            TraceContextTextMapPropagator().inject(carrier=headers)
+            return headers
+
+        # service invocation uses HTTP, so we need to inject the tracing headers into the request
+        with DaprClient(headers_callback=trace_injector) as d:
+            resp = d.invoke_method(
+                'invoke-receiver',
+                'saytrace',
+                data=request.text().encode('utf-8'),
+            )
+
+        return InvokeMethodResponse(json.dumps(resp.json()), 'application/json; charset=UTF-8')
 
 
 app.run(3001)
--- tests/clients/test_secure_http_service_invocation_client.py
+++ tests/clients/test_secure_http_service_invocation_client.py
@@ -13,19 +13,22 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import ssl
-
-from dapr.clients.http.client import DaprHttpClient
-from .certs import CERTIFICATE_CHAIN_PATH
-from .fake_http_server import FakeHttpServer
-from dapr.conf import settings
-from dapr.clients import DaprClient
-from dapr.proto import common_v1
+import typing
 from asyncio import TimeoutError
 
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.sdk.trace.sampling import ALWAYS_ON
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 
-from opencensus.trace.tracer import Tracer  # type: ignore
-from opencensus.trace import print_exporter, samplers
+from dapr.clients import DaprClient
+from dapr.clients.http.client import DaprHttpClient
+from dapr.conf import settings
+from dapr.proto import common_v1
 
+from .certs import CERTIFICATE_CHAIN_PATH
+from .fake_http_server import FakeHttpServer
 from .test_http_service_invocation_client import DaprInvocationHttpClientTests
 
 
@@ -45,7 +48,7 @@ class DaprSecureInvocationHttpClientTests(DaprInvocationHttpClientTests):
         self.server.start()
         settings.DAPR_HTTP_PORT = self.server_port
         settings.DAPR_API_METHOD_INVOCATION_PROTOCOL = 'http'
-        self.client = DaprClient('https://localhost:{}'.format(self.server_port))
+        self.client = DaprClient(f'https://localhost:{self.server_port}')
         self.app_id = 'fakeapp'
         self.method_name = 'fakemethod'
         self.invoke_url = f'/v1.0/invoke/{self.app_id}/method/{self.method_name}'
@@ -58,7 +61,7 @@ class DaprSecureInvocationHttpClientTests(DaprInvocationHttpClientTests):
     def test_global_timeout_setting_is_honored(self):
         previous_timeout = settings.DAPR_HTTP_TIMEOUT_SECONDS
         settings.DAPR_HTTP_TIMEOUT_SECONDS = 1
-        new_client = DaprClient('https://localhost:{}'.format(self.server_port))
+        new_client = DaprClient(f'https://localhost:{self.server_port}')
         self.server.set_server_delay(1.5)
         with self.assertRaises(TimeoutError):
             new_client.invoke_method(self.app_id, self.method_name, '')
@@ -66,15 +69,33 @@ class DaprSecureInvocationHttpClientTests(DaprInvocationHttpClientTests):
         settings.DAPR_HTTP_TIMEOUT_SECONDS = previous_timeout
 
     def test_invoke_method_with_tracer(self):
-        tracer = Tracer(sampler=samplers.AlwaysOnSampler(), exporter=print_exporter.PrintExporter())
+        # Create a tracer provider
+        tracer_provider = TracerProvider(sampler=ALWAYS_ON)
+
+        # Create a span processor
+        span_processor = BatchSpanProcessor(ConsoleSpanExporter())
+
+        # Add the span processor to the tracer provider
+        tracer_provider.add_span_processor(span_processor)
+
+        # Set the tracer provider
+        trace.set_tracer_provider(tracer_provider)
+
+        # Get the tracer
+        tracer = trace.get_tracer(__name__)
+
+        def trace_injector() -> typing.Dict[str, str]:
+            headers: typing.Dict[str, str] = {}
+            TraceContextTextMapPropagator().inject(carrier=headers)
+            return headers
 
         self.client = DaprClient(
-            'https://localhost:{}'.format(self.server_port),
-            headers_callback=lambda: tracer.propagator.to_headers(tracer.span_context),
+            f'https://localhost:{self.server_port}',
+            headers_callback=trace_injector,
         )
         self.server.set_response(b'FOO')
 
-        with tracer.span(name='test'):
+        with tracer.start_as_current_span(name='test'):
             req = common_v1.StateItem(key='test')
             resp = self.client.invoke_method(
                 self.app_id,
@@ -83,15 +104,13 @@ class DaprSecureInvocationHttpClientTests(DaprInvocationHttpClientTests):
                 data=req,
             )
 
-        request_headers = self.server.get_request_headers()
+        request_headers: typing.Dict[str, str] = self.server.get_request_headers()
 
         self.assertIn('Traceparent', request_headers)
         self.assertEqual(b'FOO', resp.data)
 
     def test_timeout_exception_thrown_when_timeout_reached(self):
-        new_client = DaprClient(
-            'https://localhost:{}'.format(self.server_port), http_timeout_seconds=1
-        )
+        new_client = DaprClient(f'https://localhost:{self.server_port}', http_timeout_seconds=1)
         self.server.set_server_delay(1.5)
         with self.assertRaises(TimeoutError):
             new_client.invoke_method(self.app_id, self.method_name, '')
