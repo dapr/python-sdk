ae56663cb06c76c4dde06caa8a0c798bc6f7335b
Elena Kolevska <elena@kolevska.com>
2024-02-12 16:54:53 +0000

    Linter
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- tests/clients/test_secure_dapr_grpc_client.py
+++ tests/clients/test_secure_dapr_grpc_client.py
@@ -12,10 +12,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import os
 import unittest
 from unittest.mock import patch
-import grpc  # type: ignore
 
 from dapr.clients.grpc.client import DaprGrpcClient
 from dapr.conf import settings
