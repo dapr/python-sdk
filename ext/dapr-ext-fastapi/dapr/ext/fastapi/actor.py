e5607b217c192355447de221b1aabedcf12779c2
Bernd Verst <github@bernd.dev>
2024-06-26 10:32:49 -0700

    remove unneeded import
    Signed-off-by: Bernd Verst <github@bernd.dev>

--- ext/dapr-ext-fastapi/dapr/ext/fastapi/actor.py
+++ ext/dapr-ext-fastapi/dapr/ext/fastapi/actor.py
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from typing import Any, Optional, Type, List, Callable
+from typing import Any, Optional, Type, List
 
 from fastapi import FastAPI, APIRouter, Request, Response, status  # type: ignore
 from fastapi.logger import logger
