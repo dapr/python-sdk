4802cf078625c7a660dd7df0c76666961ec76156
Elena Kolevska <elena-kolevska@users.noreply.github.com>
2024-11-11 16:34:09 +0000

    Makes streaming Subscription iterable (#751)
    * Make Streaming Subscription iterable
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * Adds iter for async client
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    * linter and updates docs
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>
    
    ---------
    
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- dapr/clients/grpc/subscription.py
+++ dapr/clients/grpc/subscription.py
@@ -143,3 +143,9 @@ class Subscription:
                     raise Exception(f'Error while closing stream: {e}')
             except Exception as e:
                 raise Exception(f'Error while closing stream: {e}')
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next_message()
