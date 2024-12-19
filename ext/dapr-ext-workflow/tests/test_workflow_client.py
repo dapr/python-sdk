b8c1f86302d5906634bd93194fd55d5f6fcb371e
Elena Kolevska <elena-kolevska@users.noreply.github.com>
2024-12-19 15:41:39 +0000

    Apply suggestions from code review
    Signed-off-by: Elena Kolevska <elena-kolevska@users.noreply.github.com>

--- ext/dapr-ext-workflow/tests/test_workflow_client.py
+++ ext/dapr-ext-workflow/tests/test_workflow_client.py
@@ -119,5 +119,3 @@ class WorkflowClientTest(unittest.TestCase):
 
             actual_resume_result = wfClient.resume_workflow(instance_id=mockInstanceId)
             assert actual_resume_result == mock_resume_result
-
-            # TODO add purge support
