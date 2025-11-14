540c6668889af28296446152656fbc3f705e8114
yaron2 <schneider.yaron@live.com>
2025-11-13 21:58:54 -0500

    use single client, minor improvements
    Signed-off-by: yaron2 <schneider.yaron@live.com>

--- dapr-ext-langgraph/dapr/ext/langgraph/dapr_checkpointer.py
+++ dapr-ext-langgraph/dapr/ext/langgraph/dapr_checkpointer.py
@@ -19,39 +19,44 @@ class DaprCheckpointer(BaseCheckpointSaver[Checkpoint]):
     def __init__(self, store_name: str, key_prefix: str):
         self.store_name = store_name
         self.key_prefix = key_prefix
+        self.client = DaprClient()
 
     # helper: construct Dapr key for a thread
     def _get_key(self, config: RunnableConfig) -> str:
         thread_id = None
+
         if isinstance(config, dict):
-            thread_id = config.get('configurable', {}).get('thread_id')
+            thread_id = config.get("configurable", {}).get("thread_id")
+
             if not thread_id:
-                thread_id = config.get('thread_id')
+                thread_id = config.get("thread_id")
+
         if not thread_id:
-            thread_id = 'default'
-        return f'{self.key_prefix}:{thread_id}'
+            thread_id = "default"
+
+        return f"{self.key_prefix}:{thread_id}"
 
     # restore a checkpoint
     def get_tuple(self, config: RunnableConfig) -> CheckpointTuple | None:
         key = self._get_key(config)
-        with DaprClient() as client:
-            resp = client.get_state(store_name=self.store_name, key=key)
-            if not resp.data:
-                return None
 
-            wrapper = json.loads(resp.data)
-            cp_data = wrapper.get('checkpoint', wrapper)
-            metadata = wrapper.get('metadata', {'step': 0})
-            if 'step' not in metadata:
-                metadata['step'] = 0
+        resp = self.client.get_state(store_name=self.store_name, key=key)
+        if not resp.data:
+            return None
 
-            cp = Checkpoint(**cp_data)
-            return CheckpointTuple(
-                config=config,
-                checkpoint=cp,
-                parent_config=None,
-                metadata=metadata,
-            )
+        wrapper = json.loads(resp.data)
+        cp_data = wrapper.get('checkpoint', wrapper)
+        metadata = wrapper.get('metadata', {'step': 0})
+        if 'step' not in metadata:
+            metadata['step'] = 0
+
+        cp = Checkpoint(**cp_data)
+        return CheckpointTuple(
+            config=config,
+            checkpoint=cp,
+            parent_config=None,
+            metadata=metadata,
+        )
 
     # save a full checkpoint snapshot
     def put(
@@ -61,27 +66,30 @@ class DaprCheckpointer(BaseCheckpointSaver[Checkpoint]):
         parent_config: RunnableConfig | None,
         metadata: dict[str, Any],
     ) -> None:
+
         key = self._get_key(config)
-        with DaprClient() as client:
-            checkpoint_serializable = {
-                'v': checkpoint['v'],
-                'id': checkpoint['id'],
-                'ts': checkpoint['ts'],
-                'channel_values': checkpoint['channel_values'],
-                'channel_versions': checkpoint['channel_versions'],
-                'versions_seen': checkpoint['versions_seen'],
-            }
-            wrapper = {'checkpoint': checkpoint_serializable, 'metadata': metadata}
 
-            # Save checkpoint to Dapr
-            client.save_state(self.store_name, key, dumps(wrapper))
+        checkpoint_serializable = {
+            'v': checkpoint['v'],
+            'id': checkpoint['id'],
+            'ts': checkpoint['ts'],
+            'channel_values': checkpoint['channel_values'],
+            'channel_versions': checkpoint['channel_versions'],
+            'versions_seen': checkpoint['versions_seen'],
+        }
 
-            # Maintain registry of all checkpoint keys
-            reg_resp = client.get_state(store_name=self.store_name, key=self.REGISTRY_KEY)
-            registry = json.loads(reg_resp.data) if reg_resp.data else []
-            if key not in registry:
-                registry.append(key)
-                client.save_state(self.store_name, self.REGISTRY_KEY, json.dumps(registry))
+        wrapper = {'checkpoint': checkpoint_serializable, 'metadata': metadata}
+
+        self.client.save_state(self.store_name, key, dumps(wrapper))
+
+        reg_resp = self.client.get_state(store_name=self.store_name, key=self.REGISTRY_KEY)
+        registry = json.loads(reg_resp.data) if reg_resp.data else []
+
+        if key not in registry:
+            registry.append(key)
+            self.client.save_state(
+                self.store_name, self.REGISTRY_KEY, json.dumps(registry)
+            )
 
     # incremental persistence (for streamed runs)
     def put_writes(
@@ -91,68 +99,70 @@ class DaprCheckpointer(BaseCheckpointSaver[Checkpoint]):
         task_id: str,
         task_path: str = '',
     ) -> None:
-        """Persist incremental updates for streaming or async workflows."""
+
+        _ = task_id, task_path
+
         key = self._get_key(config)
-        with DaprClient() as client:
-            resp = client.get_state(store_name=self.store_name, key=key)
-            if not resp.data:
-                return
 
-            wrapper = json.loads(resp.data)
-            cp = wrapper.get('checkpoint', {})
+        resp = self.client.get_state(store_name=self.store_name, key=key)
+        if not resp.data:
+            return
 
-            for field, value in writes:
-                cp['channel_values'][field] = value
+        wrapper = json.loads(resp.data)
+        cp = wrapper.get('checkpoint', {})
 
-            wrapper['checkpoint'] = cp
-            client.save_state(self.store_name, key, json.dumps(wrapper))
+        for field, value in writes:
+            cp['channel_values'][field] = value
+
+        wrapper['checkpoint'] = cp
+        self.client.save_state(self.store_name, key, json.dumps(wrapper))
 
     # enumerate all saved checkpoints
     def list(self, config: RunnableConfig) -> list[CheckpointTuple]:
-        with DaprClient() as client:
-            reg_resp = client.get_state(store_name=self.store_name, key=self.REGISTRY_KEY)
-            if not reg_resp.data:
-                return []
+        reg_resp = self.client.get_state(store_name=self.store_name, key=self.REGISTRY_KEY)
+        if not reg_resp.data:
+            return []
 
-            keys = json.loads(reg_resp.data)
-            checkpoints: list[CheckpointTuple] = []
+        keys = json.loads(reg_resp.data)
+        checkpoints: list[CheckpointTuple] = []
 
-            for key in keys:
-                cp_resp = client.get_state(store_name=self.store_name, key=key)
-                if not cp_resp.data:
-                    continue
+        for key in keys:
+            cp_resp = self.client.get_state(store_name=self.store_name, key=key)
+            if not cp_resp.data:
+                continue
 
-                wrapper = json.loads(cp_resp.data)
-                cp_data = wrapper.get('checkpoint', {})
-                metadata = wrapper.get('metadata', {})
-                cp = Checkpoint(**cp_data)
+            wrapper = json.loads(cp_resp.data)
+            cp_data = wrapper.get('checkpoint', {})
+            metadata = wrapper.get('metadata', {})
+            cp = Checkpoint(**cp_data)
 
-                checkpoints.append(
-                    CheckpointTuple(
-                        config=config,
-                        checkpoint=cp,
-                        parent_config=None,
-                        metadata=metadata,
-                    )
+            checkpoints.append(
+                CheckpointTuple(
+                    config=config,
+                    checkpoint=cp,
+                    parent_config=None,
+                    metadata=metadata,
                 )
+            )
 
-            return checkpoints
+        return checkpoints
 
     # remove a checkpoint and update the registry
     def delete_thread(self, config: RunnableConfig) -> None:
         key = self._get_key(config)
-        with DaprClient() as client:
-            client.delete_state(store_name=self.store_name, key=key)
 
-            reg_resp = client.get_state(store_name=self.store_name, key=self.REGISTRY_KEY)
-            if not reg_resp.data:
-                return
+        self.client.delete_state(store_name=self.store_name, key=key)
 
-            registry = json.loads(reg_resp.data)
-            if key in registry:
-                registry.remove(key)
-                client.save_state(
-                    store_name=self.store_name,
-                    key=self.REGISTRY_KEY,
-                    value=json.dumps(registry),
-                )
+        reg_resp = self.client.get_state(store_name=self.store_name, key=self.REGISTRY_KEY)
+        if not reg_resp.data:
+            return
+
+        registry = json.loads(reg_resp.data)
+
+        if key in registry:
+            registry.remove(key)
+            self.client.save_state(
+                store_name=self.store_name,
+                key=self.REGISTRY_KEY,
+                value=json.dumps(registry),
+            )
