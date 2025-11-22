d70a3d7f99d232433f41f4effd6ea6d6697f98e1
Casper Nielsen <casper@diagrid.io>
2025-11-21 18:28:08 -0800

    feat: add sqlite component
    Signed-off-by: Casper Nielsen <casper@diagrid.io>

--- examples/langgraph-checkpointer/main.py
+++ examples/langgraph-checkpointer/main.py
@@ -1,14 +1,14 @@
 import os
-from langchain_openai import ChatOpenAI
-from langgraph.graph import MessagesState
-from langchain_core.messages import HumanMessage, SystemMessage
-from langgraph.graph import START, StateGraph
-from langgraph.prebuilt import tools_condition, ToolNode
-from dapr.ext.langgraph import DaprCheckpointer
 
+from dapr.ext.langgraph import DaprCheckpointer
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import START, MessagesState, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
 
 load_dotenv()
+
 
 def add(a: int, b: int) -> int:
     """Adds a and b.
@@ -19,7 +19,8 @@ def add(a: int, b: int) -> int:
     """
     return a + b
 
-def multiply(a: int, b: int) -> int: 
+
+def multiply(a: int, b: int) -> int:
     """Multiply a and b.
 
     Args:
@@ -28,39 +29,59 @@ def multiply(a: int, b: int) -> int:
     """
     return a * b
 
+
 tools = [add, multiply]
-llm = ChatOpenAI(model="gpt-4o", api_key=os.environ["OPENAI_API_KEY"])
+llm = ChatOpenAI(model='gpt-4o', api_key=os.environ['OPENAI_API_KEY'])
 llm_with_tools = llm.bind_tools(tools)
 
-sys_msg = SystemMessage(content="You are a helpful assistant tasked with performing arithmetic on a set of inputs.")
+sys_msg = SystemMessage(
+    content='You are a helpful assistant tasked with performing arithmetic on a set of inputs.'
+)
+
 
 def assistant(state: MessagesState):
-   return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+    return {'messages': [llm_with_tools.invoke([sys_msg] + state['messages'])]}
+
 
 builder = StateGraph(MessagesState)
 
-builder.add_node("assistant", assistant)
-builder.add_node("tools", ToolNode(tools))
+builder.add_node('assistant', assistant)
+builder.add_node('tools', ToolNode(tools))
 
-builder.add_edge(START, "assistant")
+builder.add_edge(START, 'assistant')
 builder.add_conditional_edges(
-    "assistant",
+    'assistant',
     tools_condition,
 )
-builder.add_edge("tools", "assistant")
+builder.add_edge('tools', 'assistant')
 react_graph = builder.compile()
 
-memory = DaprCheckpointer(store_name="langgraph_checkpoint", key_prefix="dapr")
+memory = DaprCheckpointer(store_name='dapr-redis', key_prefix='dapr')
 react_graph_memory = builder.compile(checkpointer=memory)
 
-config = {"configurable": {"thread_id": "1"}}
+config = {'configurable': {'thread_id': '1'}}
 
-messages = [HumanMessage(content="Add 3 and 4.")]
-messages = react_graph_memory.invoke({"messages": messages},config)
+messages = [HumanMessage(content='Add 3 and 4.')]
+messages = react_graph_memory.invoke({'messages': messages}, config)
 for m in messages['messages']:
     m.pretty_print()
 
-messages = [HumanMessage(content="Multiply that by 2.")]
-messages = react_graph_memory.invoke({"messages": messages}, config)
+messages = [HumanMessage(content='Multiply that by 2.')]
+messages = react_graph_memory.invoke({'messages': messages}, config)
+for m in messages['messages']:
+    m.pretty_print()
+
+memory = DaprCheckpointer(store_name='dapr-sqlite', key_prefix='dapr')
+react_graph_memory = builder.compile(checkpointer=memory)
+
+config = {'configurable': {'thread_id': '2'}}
+
+messages = [HumanMessage(content='Add 5 and 6.')]
+messages = react_graph_memory.invoke({'messages': messages}, config)
+for m in messages['messages']:
+    m.pretty_print()
+
+messages = [HumanMessage(content='Multiply that by 3.')]
+messages = react_graph_memory.invoke({'messages': messages}, config)
 for m in messages['messages']:
     m.pretty_print()
