1645e1c412747d205e04eb0b09008630e5eb8539
Casper Nielsen <casper@diagrid.io>
2025-12-16 10:52:41 +0100

    Feat/ext langgraph checkpoint (#868)
    * feat: align put, put_writes and get_tuple to redis checkpoint saver for consistincy
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * chore: include new import of python-ulid for langgraph checkpoint support
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * chore: ruff formatting
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: include missing import of msgpack
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: ensure handing of empty checkpoint_id passed in config to put_writes
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * chore: align extract of config between put and put_writes
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: handle str retrieval for get_tuple with extra type checking
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * chore: add example for ext-langgraph
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: rename to redis-memory
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * feat: add sqlite component
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: remove refs to redis
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * chore: update readme with details on dual state component setup
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: add guard for None metadata
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * test: update test case to handle new output format
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * chore: remove redundant graph .compile() call
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: remove statestores and rely on existing metadata created
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: delete files not needed to align with ci testing
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: rename to agent.py & simplify
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * chore: align deps
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: correct readme to align with other examples
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * chore: add extra validation lines
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * feat: add langgraph-checkpointer to test
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * chore: ruff formatting
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: add instruction for the OPENAI_API_KEY export
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * feat: attempt using smaller llama model for examples testing
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: correct deps for ci install
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * feat: ensure ci installs required dependencies
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * chore: formatting
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: ensure ollama runs as a bg process
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: be more concise in the multiply ask
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: the small model isn't consistent enough for CI to depend on it. Add sleep to ensure ollama us serving before pulling model
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    * fix: simplify verification strings to handle non determinism in response
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>
    
    ---------
    
    Signed-off-by: Casper Nielsen <casper@diagrid.io>

---
+++ examples/langgraph-checkpointer/agent.py
@@ -0,0 +1,66 @@
+from dapr.ext.langgraph import DaprCheckpointer
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_ollama import ChatOllama
+from langgraph.graph import START, MessagesState, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+def add(a: int, b: int) -> int:
+    """Adds a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a + b
+
+
+def multiply(a: int, b: int) -> int:
+    """Multiply a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+
+tools = [add, multiply]
+llm = ChatOllama(model='llama3.2:latest')
+llm_with_tools = llm.bind_tools(tools)
+
+sys_msg = SystemMessage(
+    content='You are a helpful assistant tasked with performing arithmetic on a set of inputs.'
+)
+
+
+def assistant(state: MessagesState):
+    return {'messages': [llm_with_tools.invoke([sys_msg] + state['messages'])]}
+
+
+builder = StateGraph(MessagesState)
+
+builder.add_node('assistant', assistant)
+builder.add_node('tools', ToolNode(tools))
+
+builder.add_edge(START, 'assistant')
+builder.add_conditional_edges(
+    'assistant',
+    tools_condition,
+)
+builder.add_edge('tools', 'assistant')
+
+memory = DaprCheckpointer(store_name='statestore', key_prefix='dapr')
+react_graph_memory = builder.compile(checkpointer=memory)
+
+config = {'configurable': {'thread_id': '1'}}
+
+messages = [HumanMessage(content='Add 3 and 4.')]
+messages = react_graph_memory.invoke({'messages': messages}, config)
+for m in messages['messages']:
+    m.pretty_print()
+
+messages = [HumanMessage(content='Multiply the result by 2.')]
+messages = react_graph_memory.invoke({'messages': messages}, config)
+for m in messages['messages']:
+    m.pretty_print()
