658c6791caa48ba48ddcd34e8bdc7a4a3d36bd40
Albert Callarisa <albert@diagrid.io>
2026-01-09 14:18:59 +0100

    Fix protobuf imports
    Signed-off-by: Albert Callarisa <albert@diagrid.io>

--- dapr/proto/runtime/v1/__init__.py
+++ dapr/proto/runtime/v1/__init__.py
@@ -12,3 +12,34 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
+from .ai_pb2 import *
+from .ai_pb2_grpc import *
+from .actors_pb2 import *
+from .actors_pb2_grpc import *
+from .appcallback_pb2 import *
+from .appcallback_pb2_grpc import *
+from .binding_pb2 import *
+from .binding_pb2_grpc import *
+from .configuration_pb2 import *
+from .configuration_pb2_grpc import *
+from .crypto_pb2 import *
+from .crypto_pb2_grpc import *
+from .dapr_pb2 import *
+from .dapr_pb2_grpc import *
+from .invoke_pb2 import *
+from .invoke_pb2_grpc import *
+from .jobs_pb2 import *
+from .jobs_pb2_grpc import *
+from .lock_pb2 import *
+from .lock_pb2_grpc import *
+from .metadata_pb2 import *
+from .metadata_pb2_grpc import *
+from .pubsub_pb2 import *
+from .pubsub_pb2_grpc import *
+from .secret_pb2 import *
+from .secret_pb2_grpc import *
+from .state_pb2 import *
+from .state_pb2_grpc import *
+from .workflow_pb2 import *
+from .workflow_pb2_grpc import *
