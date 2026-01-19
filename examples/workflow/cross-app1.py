97a59adc1c464e0e1666985cb55d943a20cc5c26
joshvanl <me@joshvanl.dev>
2026-01-19 09:55:41 +0000

    tox -e ruff
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- examples/workflow/cross-app1.py
+++ examples/workflow/cross-app1.py
@@ -10,7 +10,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
 from datetime import timedelta
 
 import dapr.ext.workflow as wf
