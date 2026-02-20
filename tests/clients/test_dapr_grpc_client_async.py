435bc08fef503642c5537efe283b655508ee0882
Albert Callarisa <albert@diagrid.io>
2026-02-20 07:18:26 +0100

    Add support to bulk pubsub (#915)
    * Add support to bulk pubsub
    
    Signed-off-by: Albert Callarisa <albert@diagrid.io>
    
    * lint
    
    Signed-off-by: Albert Callarisa <albert@diagrid.io>
    
    * install dapr last
    
    Signed-off-by: Albert Callarisa <albert@diagrid.io>
    
    * Fix tox dependencies install
    
    Signed-off-by: Albert Callarisa <albert@diagrid.io>
    
    * add callback logic
    
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>
    
    * Fix bulk pubsub unit tests
    
    Signed-off-by: Albert Callarisa <albert@diagrid.io>
    
    * lint
    
    Signed-off-by: Albert Callarisa <albert@diagrid.io>
    
    * Fix type
    
    Signed-off-by: Albert Callarisa <albert@diagrid.io>
    
    * Add tests for some edge cases
    
    Signed-off-by: Albert Callarisa <albert@diagrid.io>
    
    * Add tests to cover more missed scenarios
    
    Signed-off-by: Albert Callarisa <albert@diagrid.io>
    
    ---------
    
    Signed-off-by: Albert Callarisa <albert@diagrid.io>
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>
    Co-authored-by: Cassandra Coyle <cassie@diagrid.io>
    (cherry picked from commit 4e0e38cddc18bbb4fb3d226cf92d7d0fe6061d05)

--- tests/clients/test_dapr_grpc_client_async.py
+++ tests/clients/test_dapr_grpc_client_async.py
@@ -266,6 +266,80 @@ class DaprGrpcClientAsyncTests(unittest.IsolatedAsyncioTestCase):
                 data=111,
             )
 
+    async def test_publish_events_bytes(self):
+        dapr = DaprGrpcClientAsync(f'{self.scheme}localhost:{self.grpc_port}')
+        resp = await dapr.publish_events(
+            pubsub_name='pubsub',
+            topic_name='example',
+            data=[
+                b'{"key": "value1"}',
+                b'{"key": "value2"}',
+            ],
+        )
+        self.assertEqual(0, len(resp.failed_entries))
+
+    async def test_publish_events_strings(self):
+        dapr = DaprGrpcClientAsync(f'{self.scheme}localhost:{self.grpc_port}')
+        resp = await dapr.publish_events(
+            pubsub_name='pubsub',
+            topic_name='example',
+            data=[
+                'content1',
+                'content2',
+            ],
+        )
+        self.assertEqual(0, len(resp.failed_entries))
+
+    async def test_publish_events_invalid_event_type(self):
+        dapr = DaprGrpcClientAsync(f'{self.scheme}localhost:{self.grpc_port}')
+        with self.assertRaisesRegex(ValueError, "invalid type for event <class 'dict'>"):
+            await dapr.publish_events(
+                pubsub_name='pubsub',
+                topic_name='example',
+                data=[
+                    {'entry_id': '1', 'event': 123},
+                ],
+            )
+
+    async def test_publish_events_with_failed_entries(self):
+        """Covers BulkPublishResponse with non-empty failed_entries."""
+        self._fake_dapr_server.set_bulk_publish_failed_entries_on_next_call(
+            failed_entry_count=1, error_message='simulated failure'
+        )
+        dapr = DaprGrpcClientAsync(f'{self.scheme}localhost:{self.grpc_port}')
+        resp = await dapr.publish_events(
+            pubsub_name='pubsub',
+            topic_name='example',
+            data=[b'first', b'second'],
+        )
+        self.assertEqual(1, len(resp.failed_entries))
+        self.assertEqual('simulated failure', resp.failed_entries[0].error)
+        self.assertIsNotNone(resp.failed_entries[0].entry_id)
+
+    async def test_publish_events_fallback_to_alpha1_when_stable_unimplemented(self):
+        """Covers UNIMPLEMENTED -> BulkPublishEventAlpha1 fallback in publish_events."""
+        self._fake_dapr_server.set_bulk_publish_unimplemented_on_stable_next()
+        dapr = DaprGrpcClientAsync(f'{self.scheme}localhost:{self.grpc_port}')
+        resp = await dapr.publish_events(
+            pubsub_name='pubsub',
+            topic_name='example',
+            data=[b'msg1', b'msg2'],
+        )
+        self.assertEqual(0, len(resp.failed_entries))
+
+    async def test_publish_events_raises_on_non_unimplemented_error(self):
+        """Covers non-UNIMPLEMENTED AioRpcError path in publish_events (raises DaprGrpcError)."""
+        self._fake_dapr_server.raise_exception_on_next_call(
+            status_pb2.Status(code=code_pb2.INTERNAL, message='bulk publish failed')
+        )
+        dapr = DaprGrpcClientAsync(f'{self.scheme}localhost:{self.grpc_port}')
+        with self.assertRaises(DaprGrpcError):
+            await dapr.publish_events(
+                pubsub_name='pubsub',
+                topic_name='example',
+                data=[b'msg'],
+            )
+
     async def test_subscribe_topic(self):
         # The fake server we're using sends two messages and then closes the stream
         # The client should be able to read both messages, handle the stream closure and reconnect
