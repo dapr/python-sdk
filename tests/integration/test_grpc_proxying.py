53ddae857b7aca2f9991c77e060184e1a148fe89
Sergio Herrera <627709+seherv@users.noreply.github.com>
2026-04-22 16:52:20 +0200

    Run python3 in test_grpc_proxying.py
    Signed-off-by: Sergio Herrera <627709+seherv@users.noreply.github.com>

--- tests/integration/test_grpc_proxying.py
+++ tests/integration/test_grpc_proxying.py
@@ -8,11 +8,11 @@ EXPECTED_CALLER = [
 @pytest.mark.example_dir('grpc_proxying')
 def test_grpc_proxying(dapr):
     dapr.start(
-        '--app-id invoke-receiver --app-protocol grpc --app-port 50051 --config config.yaml -- python invoke-receiver.py',
+        '--app-id invoke-receiver --app-protocol grpc --app-port 50051 --config config.yaml -- python3 invoke-receiver.py',
         wait=5,
     )
     caller_output = dapr.run(
-        '--app-id invoke-caller --dapr-grpc-port 50007 --config config.yaml -- python invoke-caller.py',
+        '--app-id invoke-caller --dapr-grpc-port 50007 --config config.yaml -- python3 invoke-caller.py',
         timeout=30,
     )
     for line in EXPECTED_CALLER:
