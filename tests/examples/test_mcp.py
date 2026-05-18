0479397ec98becc78401ddbb3579f908cc86431f
Samantha Coyle <sam@diagrid.io>
2026-05-18 09:24:33 -0500

    fix: updates for build to pass for linter/tests
    Signed-off-by: Samantha Coyle <sam@diagrid.io>

--- tests/examples/test_mcp.py
+++ tests/examples/test_mcp.py
@@ -17,6 +17,7 @@ from pathlib import Path
 from typing import Generator
 
 import pytest
+
 from tests.process_utils import get_kwargs_for_process_group, terminate_process_group
 
 REPO_ROOT = Path(__file__).resolve().parent.parent.parent
