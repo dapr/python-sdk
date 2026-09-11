696984bef8dc43e840aaf1f95fe51f2fccdaae8f
Md. Amdadul Bari Imad <amdadulbari@gmail.com>
2026-09-11 15:02:18 +0000

    test: cover 1999ms and 1001ms in dapr duration round-trip
    Add boundary cases requested in review to
    test_convert_timedelta_to_dapr_duration_subsecond: 1999ms verifies a
    fraction just under a second is not rounded up to the next whole second,
    and 1001ms verifies a tiny fraction is not dropped from the ms field.
    
    Signed-off-by: Md. Amdadul Bari Imad <amdadulbari@gmail.com>

--- tests/serializers/test_util.py
+++ tests/serializers/test_util.py
@@ -70,6 +70,16 @@ class UtilTests(unittest.TestCase):
         self.assertEqual(duration, '0h0m1s500ms0μs')
         self.assertEqual(convert_from_dapr_duration(duration), timedelta(milliseconds=1500))
 
+        # A fraction just under a second must not round up to the next whole second.
+        duration = convert_to_dapr_duration(timedelta(milliseconds=1999))
+        self.assertEqual(duration, '0h0m1s999ms0μs')
+        self.assertEqual(convert_from_dapr_duration(duration), timedelta(milliseconds=1999))
+
+        # A tiny fraction must not be dropped from the ms field.
+        duration = convert_to_dapr_duration(timedelta(milliseconds=1001))
+        self.assertEqual(duration, '0h0m1s1ms0μs')
+        self.assertEqual(convert_from_dapr_duration(duration), timedelta(milliseconds=1001))
+
     def test_convert_invalid_duration_string(self):
         TESTSTRING = '4h15m40s123ms35μshello'
         with self.assertRaises(ValueError) as exeception_context:
